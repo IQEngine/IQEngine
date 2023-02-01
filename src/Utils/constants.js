04c6a1b18b43a51080519d0cb41bce1f024f2300
Marc Lichtman <marclichtman@microsoft.com>
2023-02-01 02:51:41 -0500

    lowered max

--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -4,4 +4,4 @@
 
 export const TILE_SIZE_IN_BYTES = 50 * 1024 * 4; // must lead to doing a power of 2 FFT, and we *4 for datatypes with 4 bytes per sample
 export const MINIMUM_SCROLL_HANDLE_HEIGHT = 10; // pixels
-export const MAX_SIMULTANEOUS_FETCHES = 100; // when you drag the scrollbar it can accumulate many real quick, this limits them
+export const MAX_SIMULTANEOUS_FETCHES = 48; // when you drag the scrollbar it can accumulate many real quick, this limits them
