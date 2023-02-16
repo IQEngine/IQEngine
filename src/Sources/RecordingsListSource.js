91951fd82265fd0a38930f89773cf124199bf101
Marc Lichtman <marcll@vt.edu>
2023-02-16 14:45:51 +0900

    fetch recordings list is now much faster

--- src/Sources/RecordingsListSource.js
+++ src/Sources/RecordingsListSource.js
@@ -11,6 +11,8 @@ import parseMeta from '../Utils/parseMeta';
 
 const { BlobServiceClient } = require('@azure/storage-blob');
 
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
 async function blobToString(blob) {
   const fileReader = new FileReader();
   return new Promise((resolve, reject) => {
@@ -20,19 +22,6 @@ async function blobToString(blob) {
     fileReader.onerror = reject;
     fileReader.readAsText(blob);
   });
-}
-
-// get the length of the data file. Since some recordings may be broken, we need to return 0 by default
-async function getLength(containerClient, name) {
-  try {
-    const dataBlobName = name + '.sigmf-data';
-    const blobDataClient = containerClient.getBlobClient(dataBlobName);
-    const properties = await blobDataClient.getProperties();
-    const numBytes = properties.contentLength;
-    return { length: numBytes, client: blobDataClient };
-  } catch (_) {
-    return { length: 0, client: null };
-  }
 }
 
 export const FetchRecordingsList = (connection) => async (dispatch) => {
@@ -51,38 +40,71 @@ export const FetchRecordingsList = (connection) => async (dispatch) => {
   // List the blob(s) in the container.
   const entries = [];
   try {
-    const blob_names = [];
-    for await (const i of containerClient.listBlobsFlat()) blob_names.push(i.name);
-    for await (const blob of containerClient.listBlobsFlat()) {
+    const blobNames = [];
+    for await (const i of containerClient.listBlobsFlat()) blobNames.push(i.name);
+
+    for (let blobName of blobNames) {
       // only process meta-data files, and only ones that have a data file to go with them
-      if (blob.name.split('.').pop() === 'sigmf-meta') {
-        if (!blob_names.includes(blob.name.split('.')[0] + '.sigmf-data')) {
-          console.log('Missing data file to go with', blob.name);
-          continue;
-        }
-        const blobClient = containerClient.getBlobClient(blob.name);
-        const downloadBlockBlobResponse = await blobClient.download(); // get meta content
-
-        const json_string = await blobToString(await downloadBlockBlobResponse.blobBody);
-        // entries is a list of .sigmf-meta files, including the /'s for ones inside dirs, later on we tease them out
-        const fName = blob.name.split('.')[0];
-
-        const recording = parseMeta(json_string, baseUrl, fName, null, null);
-        if (!recording) continue;
-
-        const { length, client } = await getLength(containerClient, fName);
-        let lengthInMillionIQSamples = length / 2 / recording.bytesPerSample / 1e6;
-        recording['lengthInMillionIQSamples'] = Math.round(lengthInMillionIQSamples * 1000) / 1000;
-        recording['lengthInBytes'] = length;
-        recording['dataClient'] = client;
-
-        if (recording) {
-          entries.push(recording);
-        }
+      if (blobName.split('.').pop() !== 'sigmf-meta') {
+        entries.push(null);
+        continue;
       }
+      if (!blobNames.includes(blobName.split('.')[0] + '.sigmf-data')) {
+        console.log('Missing data file to go with', blobName);
+        entries.push(null);
+        continue;
+      }
+
+      // entries is a list of .sigmf-meta files, including the /'s for ones inside dirs, later on we tease them out
+      const fName = blobName.split('.')[0];
+
+      const blobClient = containerClient.getBlobClient(blobName);
+
+      // get meta content.  use .then() instead of await so it doesnt wait sequentially
+      let recording = null;
+      blobClient
+        .download()
+        .then(function (downloadBlockBlobResponse) {
+          return downloadBlockBlobResponse.blobBody;
+        })
+        .then(function (blobBody) {
+          return blobToString(blobBody);
+        })
+        .then(function (jsonString) {
+          recording = parseMeta(jsonString, baseUrl, fName, null, null);
+          const blobDataClient = containerClient.getBlobClient(fName + '.sigmf-data');
+          recording['dataClient'] = blobDataClient;
+          return blobDataClient.getProperties();
+        })
+        .then(function (properties) {
+          if (!recording) {
+            entries.push(null);
+            return;
+          }
+          recording['lengthInBytes'] = properties.contentLength;
+          const lengthInMillionIQSamples = properties.contentLength / 2 / recording.bytesPerSample / 1e6;
+          recording['lengthInMillionIQSamples'] = Math.round(lengthInMillionIQSamples * 1000) / 1000;
+          entries.push(recording); // cant gaurantee what order this happens in
+        });
     }
+
+    // Wait for all .then's spawned in the for loop to finish before we call it done
+    console.log('Fetching recordings list');
+    while (entries.length !== blobNames.length) {
+      await delay(100); // wait 100ms before checking again
+    }
+
+    console.log('Done fetching recordings list');
   } catch (error) {
+    console.log('GOT AN ERROR IN FetchRecordingsList:');
+    console.log(error);
     dispatch(fetchRecordingsListFailure(error));
   }
-  dispatch(fetchRecordingsListSuccess(entries));
+
+  // remove null entries
+  const final_entries = entries.filter((elements) => {
+    return elements !== null;
+  });
+
+  dispatch(fetchRecordingsListSuccess(final_entries));
 };
