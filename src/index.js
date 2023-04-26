35a556b1b8354249e0c46a98ed9c9bf0de8a0237
Marc Lichtman <marclichtman@microsoft.com>
2023-04-25 23:24:53 -0400

    Downgrade to React 17

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 // Licensed under the MIT License
 
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { render } from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootswatch/dist/darkly/bootstrap.min.css'; // needs to come before any css imports
 import './index.css';
@@ -13,14 +13,14 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './Store/store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const root = document.getElementById("root");
+render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>
-);
+, root);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
