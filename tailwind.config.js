d8d909495f95775b7e5b01daec58556423830386
Jade Wilson <jadewilson@microsoft.com>
2023-05-11 13:31:26 +0100

    Seperate tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-
-    "./src/Components/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: [ "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
