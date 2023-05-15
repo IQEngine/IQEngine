cfaafdaeb51c85dbb5c83782255831dc7ff9797a
Paul Foster <paulfo@microsoft.com>
2023-05-15 14:04:48 +0100

    Completed code for simple test

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,18 +1,6 @@
 import pytest
 from flask import Flask
-
-def create_app(test_config=None):
-    app = Flask(__name__, static_folder='./build', static_url_path='/')
-
-    @app.route('/api/status')
-    def get_status():
-        return "OK"
-
-    @app.route('/')
-    def index():
-        return app.send_static_file('index.html')
-    
-    return app
+from api.api import create_app
 
 @pytest.fixture
 def app():
