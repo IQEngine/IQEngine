007649b693a3822410015281cb6b16465810709e
Jade Wilson <jadewilson@microsoft.com>
2023-05-17 22:35:40 +0100

    Remove class name

--- client/src/Components/Button/Button.tsx
+++ client/src/Components/Button/Button.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
-  className: string;
 }
 
-export const Button = ({ children, className = '', ...rest }: ButtonProps) => {
+export const Button = ({ children, ...rest }: ButtonProps) => {
   return (
     <button className="px-3 py-2 font-medium text-white bg-zinc-800 hover:bg-zinc-700 rounded-md" {...rest}>
       {children}
