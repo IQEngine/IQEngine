6eb067754676903f10dd4e9af43d09f617152d04
hannahlspencer <hannahlspencer@gmail.com>
2023-05-19 07:24:37 +0100

    Add linters

--- api/api.py
+++ api/api.py
@@ -1,18 +1,19 @@
 from flask import Flask
 
-def create_app():
-    app = Flask(__name__, static_folder='./build', static_url_path='/')
 
-    @app.route('/api/status')
+def create_app():
+    app = Flask(__name__, static_folder="./build", static_url_path="/")
+
+    @app.route("/api/status")
     def get_status():
         return "OK"
 
-    @app.route('/')
+    @app.route("/")
     def index():
-        return app.send_static_file('index.html')
-    
+        return app.send_static_file("index.html")
+
     return app
-    
+
 
 if "__Name__" == "__main__":
     app = create_app()
