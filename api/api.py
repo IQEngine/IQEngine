c495bd417bdf2d9337dd2dbb0bb215773d6b4e80
Paul Foster <paulfo@microsoft.com>
2023-05-22 20:03:15 +0100

    Remove duplicate route

--- api/api.py
+++ api/api.py
@@ -12,10 +12,6 @@ metadata_versions = db['versions']
 
 def create_app():
     app = Flask(__name__, static_folder='./build', static_url_path='/')
-
-    @app.route('/')
-    def index():
-        return app.send_static_file('index.html')
     
     @app.route('/api/datasources', methods=['POST'])
     def create_datasource():
