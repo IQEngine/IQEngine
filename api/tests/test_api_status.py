7dbdf1c712de861d52b38b27a9a7889ec8aee3f8
t o b e <8226225+tobybrad@users.noreply.github.com>
2023-05-22 22:00:57 +0100

    All routes basic tests

--- api/tests/test_api_status.py
+++ api/tests/test_api_status.py
@@ -9,7 +9,7 @@ def test_api_returns_ok(client):
 
 def test_api_post_meta(client):
     response = client.post('/api/datasources/source_id/file_path/meta', json = {})
-    assert response.status_code == 200
+    assert response.status_code == 201
 
 def test_api_post_existing_meta(client):
     response = client.post('/api/datasources/source_id/file_path/meta', json = {})
@@ -25,3 +25,37 @@ def test_api_put_meta(client):
 def test_api_put_meta_not_existing(client):
     response = client.put('/api/datasources/source_id/file_path/meta', json = {})
     assert response.status_code == 404 
+
+def test_api_get_meta(client):
+    response = client.post('/api/datasources/source_id/file_path/meta', json = { "test" : "string"})
+    response = client.get('/api/datasources/source_id/file_path/meta')
+    assert response.status_code == 200
+    assert response.json["version_number"] == 0
+    assert response.json["test"] == "string"
+
+def test_api_get_meta_not_existing(client):
+    response = client.get('/api/datasources/source_id/file_path/meta')
+    assert response.status_code == 404
+
+def test_api_get_all_meta(client):
+    response = client.get('/api/datasources/source_id/meta')
+    assert response.status_code == 200
+    assert len(response.json["metadata"]) == 0
+    response = client.post('/api/datasources/source_id/record_a/meta', json = { "record" : "a"})
+    response = client.post('/api/datasources/source_id/record_b/meta', json = { "record" : "b"})
+    response = client.get('/api/datasources/source_id/meta')
+    assert response.status_code == 200
+    assert len(response.json["metadata"]) == 2
+
+def test_api_create_datasource(client):
+    response = client.post('/api/datasources', json = { "filepath" : "record_a" })
+    assert response.status_code == 201
+
+def test_api_get_datasources(client):
+    response = client.get('/api/datasources')
+    assert response.status_code == 200
+    assert len(response.json["datasources"]) == 0
+    response = client.post('/api/datasources', json = { "filepath" : "record_a" })
+    response = client.get('/api/datasources')
+    assert response.status_code == 200
+    assert len(response.json["datasources"]) == 1
