a1b0c1101e232960257897b5a51ab398edfd9059
Paul Foster <paulfo@microsoft.com>
2023-05-22 20:03:15 +0100

    Remove duplicate route

--- api/api.py
+++ api/api.py
@@ -24,10 +24,6 @@ def create_app(db_client = None):
     db = db_client["RFDX"]
  
     app = Flask(__name__, static_folder='./build', static_url_path='/')
-
-    @app.route('/')
-    def index():
-        return app.send_static_file('index.html')
     
     @app.route('/api/datasources', methods=['POST'])
     def create_datasource():
