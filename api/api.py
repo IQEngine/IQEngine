d91a84e02d22f6b229a7df433e873ed2272015a6
hannahlspencer <hannahlspencer@gmail.com>
2023-05-22 10:34:14 +0100

    Linter fixes

--- api/api.py
+++ api/api.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__, static_folder="./build", static_url_path="/")
-    
+
     @app.route("/")
     def index():
         return app.send_static_file("index.html")
@@ -15,7 +15,7 @@ def create_app():
     @app.route("/api/status")
     def get_status():
         return "OK"
-      
+
     return app
 
 
