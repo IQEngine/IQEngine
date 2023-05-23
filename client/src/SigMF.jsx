67b4440f6808d8157c31ccaac5eee02a2c60e9ac
Marc Lichtman <marcll@vt.edu>
2023-05-23 01:28:04 -0400

    tweaks

--- client/src/SigMF.jsx
+++ client/src/SigMF.jsx
@@ -98,7 +98,7 @@ export const SigMF = () => {
         is fairly long and difficult to skim, so the following is a summary of the important parts, leaving out the
         rarely used corner-cases.
         <br></br>
-        <br></br>A single SigMF Recording consists of a binary file containing the IQ samples using either ints or
+        <br></br>A single "SigMF Recording" consists of a binary file containing the IQ samples using either ints or
         floats, and a plaintext file containing the metadata using JSON as the format. They must be named xxx.sigmf-data
         and xxx.sigmf-meta respectively where xxx is the base file name, it can be whatever you want but it has to match
         between the two files; that is the SigMF way of saying they go together. If you have a receiver with multiple
@@ -108,35 +108,35 @@ export const SigMF = () => {
           .sigmf-collection
         </a>{' '}
         file to connect them all together, but it's also acceptable to simply include the subchannel number or
-        polarization in the filename, and if there are a lot of channels you will want to put them in their own
-        directory. The SigMF standard does not specify how to perform data compression (e.g., gzip), which means
+        polarization in the filename, and if there are a lot of channels you may put them in their own directory. The
+        SigMF standard does <b>not</b> specify how to perform data compression (e.g., gzip), which means
         software/tooling that uses SigMF is unlikely going to be able to perform the decompression for you, so you can
-        choose to do compression however you like. The general recommendation for binary IQ files is before worrying
-        about compression, first make sure that you are representing your RF recordings using the minimum bit depth,
-        e.g., instead of float32 try int16 or even int8. If compressing your files leads to a large savings in space,
-        then you probably could have achieved most of that gain by just using a lower bit depth in the first place. As
-        such, it is most common to not use any data compression.
+        choose to do compression however you like. The general recommendation for binary IQ files is to not compress;
+        before worrying about compression, first make sure that you are representing your RF recordings using the
+        minimum bit depth, e.g., instead of float32 try int16 or even int8. If compressing your files leads to a large
+        savings in space, then you probably could have achieved most of that gain by just using a lower bit depth in the
+        first place. As such, it is most common to not use any data compression.
         <br></br>
         <br></br>
-        Diving into the .sigmf-meta file, this JSON file is made up of three parts:
+        The .sigmf-meta file containing JSON is made up of three parts:
         <ul class="list-disc px-16 my-4">
           <li>
             <b>global</b> - Contains datatype, SigMF version, sample_rate, description, and other optional fields such
-            as the description of the hardware used to make the Recording. The datatype indicates float (f), signed int
-            (i), unsigned int (u) and examples include cf32_le for complex 32-bit floats (equivalent to numpy's
-            complex64) and ci16_le for the common int16 using signed ints for each I and Q. The "c" means complex and
-            the "_le" means little-endian, both of which are the standard for binary IQ files.
+            as the hardware used to make the recording. The datatype string indicates float (f), signed int (i),
+            unsigned int (u) and examples include cf32_le for complex 32-bit floats (equivalent to numpy's complex64)
+            and ci16_le for the common int16 using signed ints for each I and Q. The "c" means complex and the "_le"
+            means little-endian, both of which are the standard for binary IQ files.
           </li>
           <li>
             <b>captures</b> - A list of "captures" where each capture includes the center (RF) frequency and
-            sample_start which is the sample index the capture starts on. Typically recordings will only have 1 capture,
-            so sample_start will be 0 and the center frequency will be the important piece of information. Multiple
-            captures are most commonly seen when you frequency hop within the same recording. The optional field
-            datetime indicates the date and time corresponding to the start of each capture, using the ISO 8601 format
-            (e.g., 2022-03-17T16:43:30Z).
+            sample_start, which is the sample index the capture starts on. Typically recordings will only have one
+            capture, so sample_start will be 0 and the center frequency will be the important piece of information.
+            Multiple captures are most commonly seen when you frequency hop within the same recording. The optional
+            field datetime indicates the date and time corresponding to the start of each capture, using the ISO 8601
+            format (e.g., 2022-03-17T16:43:30Z), letting you timestamp your RF recording.
           </li>
           <li>
-            <b>annotations</b> - A list of "annotations" where each annotation is like a bounding box in time and
+            <b>annotations</b> - A list of "annotations" where each annotation is essentially a bounding box in time and
             frequency. Each annotation includes sample_start, sample_count, freq_lower_edge, freq_upper_edge. There are
             many other{' '}
             <a
@@ -149,21 +149,21 @@ export const SigMF = () => {
           </li>
         </ul>
         An example of a .sigmf-meta file is shown below:
-        <pre className="text-sm mx-32 mb-4">
+        <pre className="text-base mx-32 mb-4">
           <div dangerouslySetInnerHTML={{ __html: exampleMeta }} />
         </pre>
-        As you can see, it's fairly readable, but most importantly it's extremely machine readable and easy to parse.
+        As you can see, it's fairly readable, but most importantly it's extremely machine readable and easy to parse!
         <br></br>
         <br></br>
         The core SigMF standard can support much more than what has been presented above, and for use-cases that it
-        doesn't support, there may be a SigMF extension that does! SigMF extensions add functionality to SigMF, often
-        specific to a certain type of RF application or protocol. Many extenions live within the SigMF{' '}
+        doesn't support, there may be a SigMF "extension" that does! SigMF extensions add functionality to SigMF, often
+        specific to a certain type of RF application or protocol. Many extensions live within the SigMF{' '}
         <a href="https://github.com/sigmf/SigMF/tree/sigmf-v1.x/extensions">main repo</a> while others live in external
         repos, such as <a href="https://github.com/NTIA/sigmf-ns-ntia">NTIA's extensions.</a> As a quick example of why
         an extension might be useful, the{' '}
         <a href="https://github.com/sigmf/SigMF/blob/sigmf-v1.x/extensions/signal.sigmf-ext.md">Signal</a> extension is
-        useful for modulation/signal classification, as it adds optional fields for annotations that include
-        enumerations for the standard modulation schemes, multiplexing schemes, spread spectrum, cellular, etc.
+        great for modulation/signal classification, as it adds optional fields within annotations that include
+        enumerations for the standard modulation schemes, multiplexing schemes, spread spectrum, cellular types, etc.
         <br></br>
         <br></br>
         In addition to saving your RF recordings using the SigMF standard, you can use SigMF within your software, using
@@ -187,6 +187,8 @@ export const SigMF = () => {
             IQEngine.
           </li>
         </ul>
+        By using SigMF to store and share your RF recordings, you can avoid dataset bitrot while promoting collaboration
+        and interoperability!
       </p>
 
       {/* Schema */}
