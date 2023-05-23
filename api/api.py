c485287acf768371246e41cb24fdfb87aa10d236
Paul Foster <paulfo@microsoft.com>
2023-05-23 12:34:45 +0100

    Reinstate post for insert, revert upsert

--- api/api.py
+++ api/api.py
@@ -26,16 +26,24 @@ def create_app(db_client = None):
         db_client = create_db_client()
     db = db_client["RFDX"]
  
-    """app = Flask(__name__, static_folder='./build', static_url_path='/')
-    api = Api(app)
-    """
-
-    app = FastAPI()
-
-    import status
-    import datasources
-
-    """
+    app = Flask(__name__, static_folder='./build', static_url_path='/')
+    
+    @app.route('/api/datasources', methods=['POST'])
+    def create_datasource():
+        datasource = request.json
+        # TODO: Validate input, what about collisions?
+        datasource_id = db.datasources.insert_one(datasource).inserted_id
+        return str(datasource_id), 201
+    
+    @app.route('/api/datasources', methods=['GET'])
+    def get_all_datasources():
+        datasources = db.datasources.find()
+        result = []
+        for datasource in datasources:
+            datasource['_id'] = str(datasource['_id']) 
+            result.append(datasource)
+        return result
+    
     @app.route('/api/datasources/<datasource_id>/meta', methods=['GET'])
     def get_all_meta(datasource_id):
         metadata = db.metadata.find({'datasource_id': datasource_id})
@@ -43,8 +51,8 @@ def create_app(db_client = None):
         for datum in metadata:
             datum['_id'] = str(datum['_id'])
             result.append(datum)
-        return {"metadata": result}
-
+        return result
+    
     @app.route('/api/datasources/<datasource_id>/<filepath>/meta', methods=['GET'])
     def get_meta(datasource_id, filepath):
         metadata = db.metadata.find_one({'datasource_id': datasource_id, 'filepath': filepath})
@@ -52,24 +60,23 @@ def create_app(db_client = None):
             return "Not found", 404
         metadata['_id'] = str(metadata['_id'])
         return metadata
-    """ 
-    """
+    
     @app.route('/api/datasources/<datasource_id>/<filepath>/meta', methods=['POST'])
     def create_meta(datasource_id, filepath):
         exists = db.metadata.find_one({'datasource_id': datasource_id, 'filepath': filepath})
         if exists:
             return {"error":"record already exists"}, 400
         else:
-            metadata = request.json
-            metadata['datasource_id'] = datasource_id
-            metadata['filepath'] = filepath
-            metadata['version_number'] = 0
-            metadata_id = db.metadata.insert_one(metadata).inserted_id
-            # I wonder if this should be json.dumps()
-            return str(metadata_id), 201
-    """
-
-    """ 
+            initial_version = {
+                'version_number': 0,
+                'datasource_id': datasource_id,
+                'filepath': filepath,
+                'metadata': request.json
+            }
+            result = db.metadata.insert_one(initial_version)
+            db.versions.insert_one(initial_version)
+            return "Success",201
+   
     def get_latest_version(datasource_id, filepath):
         # Isn't latest version always current version? i.e. in metadata and not versions
         cursor = db.versions.find({'datasource_id': datasource_id, 'filepath': filepath}).sort('version', -1).limit(1)
@@ -78,24 +85,14 @@ def create_app(db_client = None):
             return None
         else:
             return result[0]
-
-    def insert_document(datasource_id, filepath, data):
-        initial_version = {
-            'version_number': 0,
-            'datasource_id': datasource_id,
-            'filepath': filepath,
-            'metadata': data
-        }
-        result = db.metadata.insert_one(initial_version)
-        db.versions.insert_one(initial_version)
-        return "Success",201
     
     @app.route('/api/datasources/<datasource_id>/<filepath>/meta', methods=['PUT'])
-    def upsert_meta(datasource_id, filepath):
-        latest_version = get_latest_version(datasource_id, filepath)
-        if latest_version == None:
-          return insert_document(datasource_id=datasource_id,filepath=filepath,data=request.json)
+    def update_meta(datasource_id, filepath):
+        exists = db.metadata.find_one({'datasource_id': datasource_id, 'filepath': filepath})
+        if exists == None:
+            return {"error":"record does not exists"}, 400
         else:
+            latest_version = get_latest_version(datasource_id, filepath)
 
             # This is going to be a race condition
             version_number = latest_version['version_number'] + 1
@@ -111,7 +108,6 @@ def create_app(db_client = None):
             result = db.versions.insert_one(new_version)
             result = db.metadata.update_one({'_id': doc_id}, {'$set': {'metadata': request.json, 'version_number': version_number}})
             return "Success", 204
-    """
 
     return app
     
--- api/tests/test_api_status.py
+++ api/tests/test_api_status.py
@@ -7,32 +7,30 @@ def test_api_returns_ok(client):
     assert response.status_code == 200 
     assert response.json() == "OK"
 
-"""
+
 def test_api_post_meta(client):
     response = client.post('/api/datasources/source_id/file_path/meta', json = {})
     assert response.status_code == 201
-"""
-"""
+
+
 def test_api_post_existing_meta(client):
     response = client.post('/api/datasources/source_id/file_path/meta', json = {})
     response = client.post('/api/datasources/source_id/file_path/meta', json = {})
     assert response.status_code == 400
-"""
+
 
 #@pytest.mark.skip()
 def test_api_put_meta(client):
-    #response = client.post('/api/datasources/source_id/file_path/meta', json = {})
-    response = client.put('/api/datasources/source_id/file_path/meta', json = { "test" : "string" })
-    assert response.status_code == 201
-    response = client.put('/api/datasources/source_id/file_path/meta', json = { "something" : "else"})
+    response = client.post('/api/datasources/source_id/file_path/meta', json = {})
+    response = client.put('/api/datasources/source_id/file_path/meta', json = {})
     assert response.status_code == 204 
 
 def test_api_put_meta_not_existing(client):
     response = client.put('/api/datasources/source_id/file_path/meta', json = {})
-    assert response.status_code == 200 #Inserts new meta 
+    assert response.status_code == 400
 
 def test_api_get_meta(client):
-    response = client.put('/api/datasources/source_id/file_path/meta', json = { "test" : "string"})
+    response = client.post('/api/datasources/source_id/file_path/meta', json = { "test" : "string"})
     response = client.get('/api/datasources/source_id/file_path/meta')
     assert response.status_code == 200
     assert response.json["version_number"] == 0
