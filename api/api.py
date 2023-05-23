2fad837b2ebb7d5432e50691804290f3781def3b
Paul Foster <paulfo@microsoft.com>
2023-05-23 12:34:45 +0100

    Reinstate post for insert, revert upsert

--- api/api.py
+++ api/api.py
@@ -33,7 +33,7 @@ def create_app(db_client = None):
         for datasource in datasources:
             datasource['_id'] = str(datasource['_id']) 
             result.append(datasource)
-        return {"datasources": result}
+        return result
     
     @app.route('/api/datasources/<datasource_id>/meta', methods=['GET'])
     def get_all_meta(datasource_id):
@@ -42,8 +42,8 @@ def create_app(db_client = None):
         for datum in metadata:
             datum['_id'] = str(datum['_id'])
             result.append(datum)
-        return {"metadata": result}
-
+        return result
+    
     @app.route('/api/datasources/<datasource_id>/<filepath>/meta', methods=['GET'])
     def get_meta(datasource_id, filepath):
         metadata = db.metadata.find_one({'datasource_id': datasource_id, 'filepath': filepath})
@@ -52,21 +52,21 @@ def create_app(db_client = None):
         metadata['_id'] = str(metadata['_id'])
         return metadata
     
-    """
     @app.route('/api/datasources/<datasource_id>/<filepath>/meta', methods=['POST'])
     def create_meta(datasource_id, filepath):
         exists = db.metadata.find_one({'datasource_id': datasource_id, 'filepath': filepath})
         if exists:
             return {"error":"record already exists"}, 400
         else:
-            metadata = request.json
-            metadata['datasource_id'] = datasource_id
-            metadata['filepath'] = filepath
-            metadata['version_number'] = 0
-            metadata_id = db.metadata.insert_one(metadata).inserted_id
-            # I wonder if this should be json.dumps()
-            return str(metadata_id), 201
-    """
+            initial_version = {
+                'version_number': 0,
+                'datasource_id': datasource_id,
+                'filepath': filepath,
+                'metadata': request.json
+            }
+            result = db.metadata.insert_one(initial_version)
+            db.versions.insert_one(initial_version)
+            return "Success",201
    
     def get_latest_version(datasource_id, filepath):
         # Isn't latest version always current version? i.e. in metadata and not versions
@@ -76,24 +76,14 @@ def create_app(db_client = None):
             return None
         else:
             return result[0]
-
-    def insert_document(datasource_id, filepath, data):
-        initial_version = {
-            'version_number': 0,
-            'datasource_id': datasource_id,
-            'filepath': filepath,
-            'metadata': data
-        }
-        result = db.metadata.insert_one(initial_version)
-        db.versions.insert_one(initial_version)
-        return "Success",200
     
     @app.route('/api/datasources/<datasource_id>/<filepath>/meta', methods=['PUT'])
-    def upsert_meta(datasource_id, filepath):
-        latest_version = get_latest_version(datasource_id, filepath)
-        if latest_version == None:
-          return insert_document(datasource_id=datasource_id,filepath=filepath,data=request.json)
+    def update_meta(datasource_id, filepath):
+        exists = db.metadata.find_one({'datasource_id': datasource_id, 'filepath': filepath})
+        if exists == None:
+            return {"error":"record does not exists"}, 400
         else:
+            latest_version = get_latest_version(datasource_id, filepath)
 
             # This is going to be a race condition
             version_number = latest_version['version_number'] + 1
@@ -108,7 +98,7 @@ def create_app(db_client = None):
             }
             result = db.versions.insert_one(new_version)
             result = db.metadata.update_one({'_id': doc_id}, {'$set': {'metadata': request.json, 'version_number': version_number}})
-            return "Success", 200
+            return "Success", 204
 
     @app.route('/api/status')
     def get_status():
