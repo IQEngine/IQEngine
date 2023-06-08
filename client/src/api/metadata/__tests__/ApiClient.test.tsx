5b306af9a0fda045c17bc1c8c18de0b8058e5eb5
Paul Foster <paulfo@microsoft.com>
2023-06-08 11:49:58 +0100

    Part upgrade of APIClient sigmfMetaData object

--- client/src/api/metadata/__tests__/ApiClient.test.tsx
+++ client/src/api/metadata/__tests__/ApiClient.test.tsx
@@ -2,6 +2,7 @@ import { test } from 'vitest';
 import nock from 'nock';
 import { ApiClient } from '../ApiClient';
 import { SigMFMetadata, Annotation, CaptureSegment } from '@/Utils/sigmfMetadata';
+import { json } from 'react-router-dom';
 
 const account = 'gnuradio';
 const container = 'iqengine';
@@ -45,16 +46,17 @@ test('getMeta should return metadata', async ({ expect }) => {
     ],
   };
 
-  //let testMetadata: SigMFMetadata | null = null;
-  //testMetadata = Object.assign(new SigMFMetadata(), expectedMeta);
-  //testMetadata.annotations = testMetadata.annotations.map((annotation) => Object.assign(new Annotation(), annotation));
-  //testMetadata.captures = testMetadata.captures.map((capture) => Object.assign(new CaptureSegment(), capture));
+  let metaJson = JSON.stringify(expectedMeta);
+  let testMetadata: SigMFMetadata | null = null;
+  testMetadata = Object.assign(new SigMFMetadata(), expectedMeta);
+  testMetadata.annotations = testMetadata.annotations.map((annotation) => Object.assign(new Annotation(), annotation));
+  testMetadata.captures = testMetadata.captures.map((capture) => Object.assign(new CaptureSegment(), capture));
 
-  nock('http://localhost:3000').get(`/api/datasources/${account}/${container}/${filePath}`).reply(200, expectedMeta);
+  nock('http://localhost:3000').get(`/api/datasources/${account}/${container}/${filePath}`).reply(200, metaJson);
 
   const client = new ApiClient();
   const result = await client.getMeta(account, container, filePath);
-  expect(result).toEqual(expectedMeta);
+  expect(result).toEqual(testMetadata);
 });
 
 test('getDataSourceMeta should return an array of metadata', async ({ expect }) => {
@@ -133,22 +135,22 @@ test('getDataSourceMeta should return an array of metadata', async ({ expect }) 
     },
   ];
 
-  //   let testMetadata: SigMFMetadata[] = expectedMeta.map((meta) => {
-  //     let newMeta = Object.assign(new SigMFMetadata(), meta);
-  //     if (newMeta.annotations) {
-  //       newMeta.annotations = newMeta.annotations.map((annotation) => Object.assign(new Annotation(), annotation));
-  //     }
-  //     if (newMeta.captures) {
-  //       newMeta.captures = newMeta.captures.map((capture) => Object.assign(new CaptureSegment(), capture));
-  //     }
-  //     return newMeta;
-  //   });
+  let testMetadata: SigMFMetadata[] = expectedMeta.map((meta) => {
+    let newMeta = Object.assign(new SigMFMetadata(), meta);
+    if (newMeta.annotations) {
+      newMeta.annotations = newMeta.annotations.map((annotation) => Object.assign(new Annotation(), annotation));
+    }
+    if (newMeta.captures) {
+      newMeta.captures = newMeta.captures.map((capture) => Object.assign(new CaptureSegment(), capture));
+    }
+    return newMeta;
+  });
 
   nock('http://localhost:3000').get(`/api/datasources/${account}/${container}/meta`).reply(200, expectedMeta);
 
   const client = new ApiClient();
   const result = await client.getDataSourceMeta(account, container);
-  expect(result).toEqual(expectedMeta);
+  expect(result).toEqual(testMetadata);
 });
 
 test('updateMeta should return updated metadata', async ({ expect }) => {
@@ -180,7 +182,7 @@ test('updateMeta should return updated metadata', async ({ expect }) => {
     ],
     annotations: [
       {
-        'core:sample_start': 260780,
+        'core:sample_start': 260880,
         'core:sample_count': 285354,
         'core:freq_lower_edge': 8486138750,
         'core:freq_upper_edge': 8486243700,
@@ -231,11 +233,16 @@ test('updateMeta should return updated metadata', async ({ expect }) => {
   newMetaData.annotations = newMetaData.annotations.map((annotation) => Object.assign(new Annotation(), annotation));
   newMetaData.captures = newMetaData.captures.map((capture) => Object.assign(new CaptureSegment(), capture));
 
+  let testMetaData: SigMFMetadata | null = null;
+  testMetaData = Object.assign(new SigMFMetadata(), newMeta);
+  testMetaData.annotations = newMetaData.annotations.map((annotation) => Object.assign(new Annotation(), annotation));
+  testMetaData.captures = newMetaData.captures.map((capture) => Object.assign(new CaptureSegment(), capture));
+
   nock('http://localhost:3000')
     .put(`/api/datasources/${account}/${container}/${filePath}`, newMeta)
     .reply(200, expectedMeta);
 
   const client = new ApiClient();
   const result = await client.updateMeta(account, container, filePath, newMetaData);
-  expect(result).toEqual(expectedMeta);
+  expect(result).toEqual(testMetaData);
 });
