d880a7dfcf73a853dd41c7237f0584c243be5207
greg oliver <golive@microsoft.com>
2023-06-09 06:58:46 +0100

    increase workers

--- playwright-pr.config.ts
+++ playwright-pr.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 3 : undefined,
+  workers: process.env.CI ? 9 : undefined,
   /* Timeout 90 seconds per test */
   timeout: 90000,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
