001abf99539711ac407a1a86f33bd1baff8617c1
Jade Wilson <jade_wilson66@hotmail.co.uk>
2023-06-12 14:35:27 +0100

    Validator page (#184)
    * Strip validator page down
    
    * Separate validation into it's own function
    
    * linter fixes
    
    * Another linter fix
    
    ---------
    
    Co-authored-by: Jade Wilson <jadewilson@microsoft.com>

--- client/src/Components/Validator/Validator.tsx
+++ client/src/Components/Validator/Validator.tsx
@@ -1,22 +1,67 @@
 // Copyright (c) 2023 Marc Lichtman
 // Licensed under the MIT License
 
-import React from 'react';
-import v8Validator from '@rjsf/validator-ajv8';
-import Playground, { PlaygroundProps } from './components';
+import React, { useState } from 'react';
+import { vscodeDark } from '@uiw/codemirror-theme-vscode';
+import CodeMirror from '@uiw/react-codemirror';
+import { INITIAL_METADATA_SNIPPET } from '@/Utils/constants';
+import { metadataValidator } from '@/Utils/validators';
 
-const validators: PlaygroundProps['validators'] = {
-  AJV8: v8Validator,
-};
+export const Validator = () => {
+  const [metadata, setMetadata] = useState(INITIAL_METADATA_SNIPPET);
+  const [errors, setErrors] = useState([]);
 
-const themes: PlaygroundProps['themes'] = {
-  default: {
-    stylesheet: '//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css',
-    theme: {},
-    subthemes: {},
-  },
-};
+  const onChangeHandler = (metadataValue) => {
+    const metadata = metadataValidator(metadataValue);
+    setMetadata(metadata.metadata);
+    setErrors(metadata.errors);
+  };
 
-export default function Validator() {
-  return <Playground themes={themes} validators={validators} />;
+  return (
+    <div>
+      <div className="flex justify-center">
+        <CodeMirror
+        aria-label= 'Validator Code Editor'
+          value={metadata}
+          height="500px"
+          width="700px"
+          onChange={onChangeHandler}
+          theme={vscodeDark}
+          readOnly={false}
+        />
+      </div>
+      {errors.length > 0 && (
+        <div>
+          <div className="flex justify-center">
+            <h2 className="text-error" style={{ width: '700px' }}>
+              Errors
+            </h2>
+          </div>
+          <div className="flex justify-center text-error">
+            <ul aria-label='Validator Errors' style={{ width: '700px' }}>
+              {errors.map((error, i) => (
+                <li key={'error ' + { i }}>
+                  <svg
+                    className="w-4 h-4 mr-1.5 text-error flex-shrink-0 inline-block"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  {error.message} {error.instancePath ? ' inside ' + error.instancePath : ''}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
+
+export default Validator;
