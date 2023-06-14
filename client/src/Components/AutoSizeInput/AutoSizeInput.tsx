152f05da8676de5d6e1915fcd39fc21cc620455c
Jade Wilson <jade_wilson66@hotmail.co.uk>
2023-06-14 09:45:05 +0100

    Fix timing auto size bug (#192)
    Co-authored-by: Jade Wilson <jadewilson@microsoft.com>

--- client/src/Components/AutoSizeInput/AutoSizeInput.tsx
+++ client/src/Components/AutoSizeInput/AutoSizeInput.tsx
@@ -24,7 +24,7 @@ export const AutoSizeInput = ({
 
   useEffect(() => {
     setWidth(span.current.offsetWidth + 3);
-  }, [content]);
+  }, [span.current?.offsetWidth]);
 
   const changeHandler = useCallback((evt) => {
     setContent(evt.target.value);
