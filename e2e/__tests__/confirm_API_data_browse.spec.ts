122c9cc1a180966bea903381fe68e89ef5320b74
Paul Foster <paulfo@microsoft.com>
2023-06-16 10:16:59 +0100

    Test enhancement

--- e2e/__tests__/confirm_API_data_browse.spec.ts
+++ e2e/__tests__/confirm_API_data_browse.spec.ts
@@ -4,16 +4,16 @@ test('API Datasource Browsing', async ({ page, request }) => {
   const dataSource = {
     type: 'api',
     name: 'Test API DataSource',
-    account: 'test_account' + Math.random(),
+    account: 'test_account',
     container: 'test_container',
     description: 'test description',
     imageURL: 'https://i.etsystatic.com/20456772/r/il/a94e13/3604976065/il_1588xN.3604976065_dzyi.jpg',
   };
 
-  const response = await request.post('/api/datasources', { data: dataSource });
-  expect(response.status()).toBe(201); // Returns between 200 and 201 depending on local or staging environment
+  const response = await request.post('/api/datasources', { data: dataSource }); // Returns between 200 and 201 depending on local or staging environment
 
   await page.goto('/');
-  await page.locator('[id="TestAPIDataSource"]').click(); // Fails due to multiple elements with the same id after first run
+  const elements = await page.locator('[id="TestAPIDataSource"]'); // After first run there will be more than one element by this name
+  await elements.nth(0).click(); // Only look for the first element
   await expect(page.getByText('AUTHOR')).toBeVisible();
 });
