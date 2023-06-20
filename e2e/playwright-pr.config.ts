ef8261c5700364e61ceb73732fe99fc45657bf62
Hannah <hannahlspencer@gmail.com>
2023-06-20 14:46:46 +0100

    Update staging url (#228)

--- e2e/playwright-pr.config.ts
+++ e2e/playwright-pr.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://iqengineweb-staging.azurewebsites.net/',
+    baseURL: 'https://staging.iqengine.org/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
