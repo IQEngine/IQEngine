e814c0beb5d9b1eb2c86bc2156fd4a3b7e3bf5ec
Hannah <hannahlspencer@gmail.com>
2023-06-21 10:23:13 +0100

    Fix playwright tests (#237)

--- e2e/__tests__/siggen.spec.ts
+++ e2e/__tests__/siggen.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('test siggen page', async ({ page }) => {
   await page.goto('/');
-  await page.getByText('Siggen').click();
+  await page.locator('[id="Siggen"]').click();
   await expect(page.getByText('Python-Based Signal Generator')).toBeVisible();
   await page.getByText('Run').click();
   // need to add some more content to the site itself to check that everything worked
