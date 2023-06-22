3f04992c0f0ac3723aaef4f00faf0880baa9860e
Jade Wilson <jadewilson@microsoft.com>
2023-06-22 11:06:38 +0100

    fix files with linter

--- api/tests/test_iq_api.py
+++ api/tests/test_iq_api.py
@@ -1,17 +1,19 @@
-from unittest.mock import patch, Mock
-from tests.test_data import test_datasource, valid_metadata
 import base64
+from unittest.mock import Mock, patch
+
+from tests.test_data import test_datasource, valid_metadata
 
 
 def test_get_iq(client):
     response = client.post("/api/datasources", json=test_datasource).json()
     response = client.post(
         f'/api/datasources/{test_datasource["account"]}/{test_datasource["container"]}/file_path/meta',
-        json=valid_metadata,)
+        json=valid_metadata,
+    )
 
     # Mock the BlobClient and its methods
-    with patch('handlers.iq.BlobClient') as MockBlobClient:
-        testBytes = b'the quick brown fox jumps over the lazy dog'
+    with patch("handlers.iq.BlobClient") as MockBlobClient:
+        testBytes = b"the quick brown fox jumps over the lazy dog"
         mock_blob = Mock()
         mock_blob.readall.return_value = testBytes
         mock_blob_client = Mock()
@@ -20,9 +22,10 @@ def test_get_iq(client):
         MockBlobClient.from_blob_url.return_value = mock_blob_client
 
         response = client.get(
-            f'/api/datasources/'
+            f"/api/datasources/"
             f'{test_datasource["account"]}/{test_datasource["container"]}'
-            f'/file_path/iqslice?offsetBytes=0&countBytes=10')
+            f"/file_path/iqslice?offsetBytes=0&countBytes=10"
+        )
 
         assert response.status_code == 200
         assert response.json() == {"data": base64.b64encode(testBytes).decode()}
@@ -32,12 +35,13 @@ def test_get_iq_data_slices(client):
     response = client.post("/api/datasources", json=test_datasource).json()
     response = client.post(
         f'/api/datasources/{test_datasource["account"]}/{test_datasource["container"]}/file_path/meta',
-        json=valid_metadata,)
+        json=valid_metadata,
+    )
 
     # Mock the BlobClient and its methods
-    with patch('handlers.iq.BlobClient') as MockBlobClient:
+    with patch("handlers.iq.BlobClient") as MockBlobClient:
         mock_blob = Mock()
-        mock_blob.readall.return_value = b'the quick brown fox jumps over the lazy dog'
+        mock_blob.readall.return_value = b"the quick brown fox jumps over the lazy dog"
         mock_blob_client = Mock()
         mock_blob_client.get_blob_properties.return_value = Mock(size=43)
         mock_blob_client.download_blob = Mock(return_value=mock_blob)
@@ -45,7 +49,7 @@ def test_get_iq_data_slices(client):
 
         response = client.post(
             f'/api/datasources/{test_datasource["account"]}/{test_datasource["container"]}/filepath/iqslices',
-            json={"indexes": [0, 1, 2], "tile_size": 2, "bytes_per_sample": 4}
+            json={"indexes": [0, 1, 2], "tile_size": 2, "bytes_per_sample": 4},
         )
         assert response.status_code == 200
         assert isinstance(response.json(), list)
