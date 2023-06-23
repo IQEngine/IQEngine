503577b3dd3be1e3c12f027ddd39231cf61b9f94
greg oliver <golive@microsoft.com>
2023-06-23 11:19:47 +0100

    lint

--- e2e/webServer.sh
+++ e2e/webServer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # runs from e2e folder
-cd ..
+cd .. || exit
 docker build -t iqengine .
 docker run -p 3000:3000 -e RFDX_FF_INMEMDB=1 -e VITE_FEATURE_FLAGS='{"useAPIDatasources": true}' iqengine:latest
-cd e2e
+cd e2e || exit
