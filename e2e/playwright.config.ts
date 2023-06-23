3be339d0945b8d46fcd4c55688343886255b19d0
greg oliver <golive@microsoft.com>
2023-06-23 10:58:52 +0100

    move database warmup to global setup step

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -9,11 +9,11 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 1,
+  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'list',
-  /* Timeout 90 seconds per test */
-  timeout: 90000,
+  /* Timeout 30 seconds per test to allow database warmup time */
+  timeout: 3000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -36,18 +36,25 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
+      name: 'setup',
+      testMatch: /global.setup\.ts/,
+    },
+    {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
+      dependencies: ['setup'],
     },
 
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
+      dependencies: ['setup'],
     },
 
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
+      dependencies: ['setup'],
     },
   ],
 });
--- e2e/webServer.sh
+++ e2e/webServer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# runs from e2e folder
 cd ..
 docker build -t iqengine .
-docker run -p 3000:3000 -e RFDX_FF_INMEMDB=1 iqengine:latest
+docker run -p 3000:3000 -e RFDX_FF_INMEMDB=1 -e VITE_FEATURE_FLAGS='{"useAPIDatasources": true}' iqengine:latest
+cd e2e
