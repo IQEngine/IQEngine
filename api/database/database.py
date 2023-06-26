8c5e1d42db4a1403c1bc0decfbcdf0c2b7528dd6
Gabriel Monteiro Nepomuceno <gabriel.bico@gmail.com>
2023-06-26 22:23:58 +0100

    Add processors endpoint to API (#262)
    * Add processors endpoint to API
    
    * Remove bug on delete
    
    * Linting
    
    * Linting

--- api/database/database.py
+++ api/database/database.py
@@ -46,3 +46,8 @@ def metadata_collection():
 def metadata_versions_collection():
     collection: Collection[Metadata] = db().versions
     return collection
+
+
+def processors_collection():
+    collection: Collection[Metadata] = db().processors
+    return collection
