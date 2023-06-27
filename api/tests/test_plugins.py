7203247ff997526fed44f17c13adb2bc1d96768e
Gabriel Monteiro Nepomuceno <gabriel.bico@gmail.com>
2023-06-27 14:55:04 +0100

    Add hook processors (#264)
    * Add hook processors
    
    * Linter
    
    * Remove dead code
    
    * Move from processors to plugins
    
    * Linting
    
    * Linting
    
    * Add duplicated name test
    
    * Linting

---
+++ api/tests/test_plugins.py
@@ -0,0 +1,105 @@
+# vim: tabstop=4 shiftwidth=4 expandtab
+# vim: tabstop=4 shiftwidth=4 expandtab
+
+
+def test_api_create_plugin(client):
+    test_plugin = {
+        "name": "test_plugin",
+        "url": "http://test_plugin.com",
+    }
+    response = client.post("/api/plugins", json=test_plugin)
+    assert response.status_code == 201
+    assert response.json() == test_plugin
+
+
+def test_api_create_plugin_cannot_duplicate(client):
+    test_plugin = {
+        "name": "test_plugin",
+        "url": "http://test_plugin.com",
+    }
+    response = client.post("/api/plugins", json=test_plugin)
+    assert response.status_code == 201
+    assert response.json() == test_plugin
+    response = client.post("/api/plugins", json=test_plugin)
+    assert response.status_code == 409
+
+
+def test_api_create_plugin_cannot_duplicate_name(client):
+    test_plugin = {
+        "name": "test_plugin",
+        "url": "http://test_plugin.com",
+    }
+    response = client.post("/api/plugins", json=test_plugin)
+    assert response.status_code == 201
+    assert response.json() == test_plugin
+    test_plugin["url"] = "http://test_plugin2.com"
+    response = client.post("/api/plugins", json=test_plugin)
+    assert response.status_code == 409
+
+
+def test_api_get_plugins(client):
+    test_plugin = {
+        "name": "test_plugin",
+        "url": "http://test_plugin.com",
+    }
+    response = client.post("/api/plugins", json=test_plugin)
+    assert response.status_code == 201
+    assert response.json() == test_plugin
+    response = client.get("/api/plugins")
+    assert response.status_code == 200
+    assert response.json() == [test_plugin]
+
+
+def test_api_get_plugin(client):
+    test_plugin = {
+        "name": "test_plugin",
+        "url": "http://test_plugin.com",
+    }
+    response = client.post("/api/plugins", json=test_plugin)
+    assert response.status_code == 201
+    assert response.json() == test_plugin
+    response = client.get("/api/plugins/test_plugin")
+    assert response.status_code == 200
+    assert response.json() == test_plugin
+
+
+def test_api_get_plugin_not_found(client):
+    response = client.get("/api/plugins/test_plugin")
+    assert response.status_code == 404
+
+
+def test_api_update_plugin(client):
+    test_plugin = {
+        "name": "test_plugin",
+        "url": "http://test_plugin.com",
+    }
+    response = client.post("/api/plugins", json=test_plugin)
+    assert response.status_code == 201
+    assert response.json() == test_plugin
+    test_plugin["url"] = "http://test_plugin2.com"
+    response = client.put("/api/plugins/test_plugin", json=test_plugin)
+    assert response.status_code == 200
+    assert response.json() == test_plugin
+
+
+def test_api_update_plugin_not_found(client):
+    test_plugin = {
+        "name": "test_plugin",
+        "url": "http://test_plugin.com",
+    }
+    response = client.put("/api/plugins/test_plugin", json=test_plugin)
+    assert response.status_code == 404
+
+
+def test_api_delete_plugine(client):
+    test_plugin = {
+        "name": "test_plugin",
+        "url": "http://test_plugin.com",
+    }
+    response = client.post("/api/plugins", json=test_plugin)
+    assert response.status_code == 201
+    assert response.json() == test_plugin
+    response = client.delete("/api/plugins/test_plugin")
+    assert response.status_code == 200
+    response = client.get("/api/plugins/test_plugin")
+    assert response.status_code == 404
