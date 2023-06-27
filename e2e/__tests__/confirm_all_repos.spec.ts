03b2cc66d7e89ff0078d2f5cbbbbffd415b0993c
greg oliver <golive@microsoft.com>
2023-06-27 15:20:43 +0100

    add test of a datasource that is listed in the api

--- e2e/__tests__/confirm_all_repos.spec.ts
+++ e2e/__tests__/confirm_all_repos.spec.ts
@@ -1,4 +1,19 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
+import { MongoClient, Db } from 'mongodb';
+import dotenv from 'dotenv';
+import dotenvExpand from 'dotenv-expand';
+
+test.beforeAll(async ({ request }) => {
+  const dataSource = {
+    type: 'api',
+    name: '(API) Test API DataSource',
+    account: 'test_account',
+    container: 'test_container',
+    description: 'test description',
+    imageURL: 'https://i.etsystatic.com/20456772/r/il/a94e13/3604976065/il_1588xN.3604976065_dzyi.jpg',
+  };
+  const response = await request.post('/api/datasources', { data: dataSource });
+});
 
 // the "Open Local Directory" and "Select 1" options are not tested
 test('Confirm all repos display', async ({ page }) => {
@@ -13,6 +28,10 @@ test('Confirm all repos display', async ({ page }) => {
   await page.locator('[id="NortheasternUniversity"]').click();
   await page.locator('[id="IQEngineLogo"]').click();
 
+  // api data source
+  await page.locator('[id="(API)TestAPIDataSource"]').click();
+  await page.locator('[id="IQEngineLogo"]').click();
+
   // signal generator
   await page.locator('[id="Siggen"]').click();
   await page.locator('[id="IQEngineLogo"]').click();
@@ -25,4 +44,16 @@ test('Confirm all repos display', async ({ page }) => {
   await page.locator('[id="AzureBlob"]').click();
   // doesn't currently change page as blob parameters required
   //await page.locator('[id="IQEngineLogo"]').click();
+});
+
+test.afterAll(async ({}) => {
+  var conn = dotenv.config();
+  dotenvExpand.expand(conn);
+
+  const connection_string = process.env.IQENGINE_METADATA_DB_CONNECTION_STRING || '';
+  const client: MongoClient = new MongoClient(connection_string);
+  await client.connect();
+  const db: Db = client.db('IQEngine');
+  const collection = db.collection('datasources');
+  collection.deleteOne({ type: 'api', name: '(API) Test API DataSource' });
 });
