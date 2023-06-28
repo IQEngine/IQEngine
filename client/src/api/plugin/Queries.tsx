6231a351c3f8add85885ed9c443dafe99e8eaeed
Gabriel Nepomuceno <gabriel.bico@gmail.com>
2023-06-28 06:25:39 +0100

    Adding UI changes for plugin

--- client/src/api/plugin/Queries.tsx
+++ client/src/api/plugin/Queries.tsx
@@ -1,19 +1,51 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import { PluginDefinition } from '../Models';
+import { PluginDefinition, PluginParameters } from '../Models';
 
 export function useGetPlugins() {
   return useQuery<PluginDefinition[]>(
     ['plugins'],
     async () => {
       const response = await axios.get<PluginDefinition[]>('/api/plugins').catch((err) => {
-        console.log('useGetPlugins error', err);
-        return { data: [] };
+        console.error('Failing getting plugins from api', err);
+        console.log('Falling back to env variable', import.meta.env.IQENGINE_PLUGINS);
+        if (import.meta.env.IQENGINE_PLUGINS) {
+          const plugins = JSON.parse(import.meta.env.IQENGINE_PLUGINS);
+          return { data: plugins };
+        } else {
+          return { data: [] };
+        }
       });
       return response.data;
     },
     {
       placeholderData: [],
+    }
+  );
+}
+
+export function useGetPlugin(plugin: PluginDefinition) {
+  return useQuery<string[]>(
+    ['plugin', plugin.name],
+    async () => {
+      const response = await axios.get<string[]>(plugin.url);
+      return response.data;
+    },
+    {
+      enabled: !!plugin,
+    }
+  );
+}
+
+export function useGetPluginParameters(pluginUrl: string) {
+  return useQuery<PluginParameters>(
+    ['plugin', 'parameters', pluginUrl],
+    async () => {
+      const response = await axios.get<PluginParameters>(pluginUrl);
+      return response.data;
+    },
+    {
+      enabled: !!pluginUrl,
     }
   );
 }
--- client/src/api/plugin/__tests__/Queries.test.tsx
+++ client/src/api/plugin/__tests__/Queries.test.tsx
@@ -44,4 +44,39 @@ describe('useGetPlugins', () => {
     await waitFor(() => expect(result.current.isFetching).toEqual(false), { timeout: 1000 });
     await waitFor(() => expect(result.current.data).toEqual(expectedPlugins), { timeout: 1000 });
   });
+
+  test('should be able to get the list of plugins from the env', async ({ expect }) => {
+    const { wrapper } = useAllProviders();
+    let expectedPlugins = [
+      {
+        name: 'builtIn',
+        url: 'http://localhost:3000/api/plugins/builtIn',
+      },
+      {
+        name: 'gnuradio',
+        url: 'http://localhost:3000/api/plugins/gnuradio',
+      },
+    ];
+    nock('http://localhost:3000').get(`/api/plugins`).reply(500, null);
+    process.env.IQENGINE_PLUGINS = JSON.stringify(expectedPlugins);
+
+    const { result } = renderHook(() => useGetPlugins(), {
+      wrapper: wrapper,
+    });
+    await waitFor(() => expect(result.current.isSuccess).toBe(true));
+    await waitFor(() => expect(result.current.isFetching).toEqual(false), { timeout: 1000 });
+    await waitFor(() => expect(result.current.data).toEqual(expectedPlugins), { timeout: 1000 });
+  });
+});
+
+describe('useGetPlugin', () => {
+  afterEach(() => {
+    nock.cleanAll();
+    const { queryClient } = useAllProviders();
+    queryClient.clear();
+  });
+
+  test('should be able to get the list of plugins from the api', async ({ expect }) => {
+    const { wrapper } = useAllProviders();
+  });
 });
