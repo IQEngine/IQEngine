c375adc9868dfe5d3422b0c6ea04f7f1005d32b1
hannahlspencer <hannahlspencer@gmail.com>
2023-06-29 14:47:14 +0100

    Lint

--- e2e/playwright-pr.config.ts
+++ e2e/playwright-pr.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
-  
+
   /* Start the API Server */
   webServer: [
     {
