4c0aff16bccf3c7260cebddf543549f5709c2cf9
greg oliver <golive@microsoft.com>
2023-06-30 08:53:22 +0100

    fix pw config

--- e2e/playwright-pr.config.ts
+++ e2e/playwright-pr.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://staging.iqengine.org/',
+    baseURL: 'https://localhost:3000/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -32,7 +32,6 @@ export default defineConfig({
       timeout: 200 * 1000,
     },
   ],
-
 
   /* Configure projects for major browsers */
   projects: [
