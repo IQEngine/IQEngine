019178a86831df8f7bd1b01b1aefdae85a444149
Gabriel Monteiro Nepomuceno <gabriel.bico@gmail.com>
2023-07-03 10:04:42 +0100

    Document file structure (#292)
    * Add file structure docs
    
    * Linting
    
    * Spelling

--- client/src/store/reducers/ConnectionReducer.ts
+++ client/src/store/reducers/ConnectionReducer.ts
@@ -5,13 +5,6 @@ import { DataSource } from '@/api/Models';
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  accountName: '',
-  containerName: '',
-  sasToken: '',
-  recording: '',
-  metafilehandle: '',
-  datafilehandle: '',
-  blobClient: null,
   dataSources: {} as Record<string, DataSource>,
 };
 
@@ -22,50 +15,12 @@ export const connectionSlicer = createSlice({
     upsertDataSource: (state, action: PayloadAction<DataSource>) => {
       const dataSourceKey = `${action.payload.account}/${action.payload.container}`;
       state.dataSources[dataSourceKey] = action.payload;
-    },
-    updateConnectionAccountName: (state, action) => {
-      state.accountName = action.payload;
-    },
-    updateConnectionContainerName: (state, action) => {
-      state.containerName = action.payload;
-    },
-    updateConnectionRecording: (state, action) => {
-      state.recording = action.payload;
-    },
-    updateConnectionSasToken: (state, action) => {
-      state.sasToken = action.payload;
-    },
-    updateConnectionMetaFileHandle: (state, action) => {
-      state.metafilehandle = action.payload;
-    },
-    updateConnectionDataFileHandle: (state, action) => {
-      state.datafilehandle = action.payload;
-    },
-    updateConnectionBlobClient: (state, action) => {
-      state.blobClient = action.payload;
-    },
-    resetConnection: (state) => {
-      state.accountName = '';
-      state.containerName = '';
-      state.sasToken = '';
-      state.recording = '';
-      state.metafilehandle = '';
-      state.datafilehandle = '';
-      state.blobClient = null;
-    },
+    }
   },
 });
 
 export const {
   upsertDataSource,
-  updateConnectionAccountName,
-  updateConnectionContainerName,
-  updateConnectionRecording,
-  updateConnectionSasToken,
-  updateConnectionMetaFileHandle,
-  updateConnectionDataFileHandle,
-  updateConnectionBlobClient,
-  resetConnection,
 } = connectionSlicer.actions;
 
 export default connectionSlicer.reducer;
