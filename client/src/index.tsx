5ff76c213728a1207c26d46b339f854b5861a90b
Jade Wilson <jade_wilson66@hotmail.co.uk>
2023-07-10 15:56:38 +0100

    Auth provider (#333)
    * Add intial admin login
    
    * Remove helpful urls
    
    * PR feedback
    
    * Remove unneeded instance
    
    * Add auth provider
    
    * fix api test
    
    ---------
    
    Co-authored-by: Jade Wilson <jadewilson@microsoft.com>

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -12,38 +12,22 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { FeatureFlagsProvider } from '@/hooks/useFeatureFlags';
 import { useIQEngineQueryClient } from '@/hooks/useIQEngineQueryClient';
 import { useIQEngineRouter } from '@/hooks/useIQEngineRouter';
-import { MsalProvider } from '@azure/msal-react';
-import { PublicClientApplication, EventType } from '@azure/msal-browser';
-import { msalConfig } from '@/authConfig';
-
-export const msalInstance = new PublicClientApplication(msalConfig);
-
-if (!msalInstance.getActiveAccount() && msalInstance.getAllAccounts().length > 0) {
-  msalInstance.setActiveAccount(msalInstance.getAllAccounts()[0]);
-}
-
-msalInstance.enableAccountStorageEvents();
-
-msalInstance.addEventCallback((event) => {
-  if (event.eventType === EventType.LOGIN_SUCCESS && event.payload.account) {
-    const account = event.payload.account;
-    msalInstance.setActiveAccount(account);
-  }
-});
+import { useIQEngineAuthProvider } from './hooks/useIQEngineAuthProvider';
 
 const container = document.getElementById('root');
 if (!container) throw new Error('No root element found');
 const root = createRoot(container);
 const { queryClient } = useIQEngineQueryClient();
-const { router } = useIQEngineRouter(msalInstance);
+const { router } = useIQEngineRouter();
+const { AuthProvider } = useIQEngineAuthProvider();
 root.render(
-  <MsalProvider instance={msalInstance}>
-    <QueryClientProvider client={queryClient}>
+  <QueryClientProvider client={queryClient}>
+    <AuthProvider>
       <FeatureFlagsProvider flags={null}>
         <RouterProvider router={router} />
       </FeatureFlagsProvider>
-    </QueryClientProvider>
-  </MsalProvider>
+    </AuthProvider>
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
