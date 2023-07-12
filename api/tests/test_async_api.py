ccad46e6e6d2f8e14b7600d166282e91c451928d
Gabriel Nepomuceno <gabriel.bico@gmail.com>
2023-07-12 13:42:19 +0100

    correct event loop

--- api/tests/test_async_api.py
+++ api/tests/test_async_api.py
@@ -6,10 +6,6 @@ from main import app
 
 from database.models import Metadata
 from tests.test_data import test_datasource, valid_metadata
-from motor.core import AgnosticClient
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
-AgnosticClient.get_io_loop = asyncio.get_event_loop
 
 
 @pytest.fixture(params=["asyncio"])
@@ -18,7 +14,7 @@ def anyio_backend(request):
 
 @pytest.mark.anyio
 async def test_api_returns_ok():
-    async with AsyncClient(app=app, base_url="http://test",loop=asyncio.new_event_loop()) as client:
+    async with AsyncClient(app=app, base_url="http://test") as client:
         response = await client.get("/api/status")
         assert response.status_code == 200
         assert response.json() == "OK"
