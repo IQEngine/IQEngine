c495e180032363326c409a1186142a3841277d7c
Jade Wilson <jadewilson@microsoft.com>
2023-07-14 14:03:51 +0100

    Remove config

--- client/src/pages/admin/pages/configuration.tsx
+++ client/src/pages/admin/pages/configuration.tsx
@@ -1,27 +1,9 @@
-import { useConfigQuery } from '@/api/config/queries';
 import React from 'react';
 
 export const Configuration = () => {
-  const config = useConfigQuery();
   return (
     <div className="card shadow-lg compact side bg-base-100">
       <h2>Configuration</h2>
-      <div className="card-body">
-        {config.data?.featureFlags &&
-          Object.keys(config.data?.featureFlags).map((key, i) => (
-            <div key={i}>
-              <div className="form-control">
-                <label className="cursor-pointer label">
-                  <span className="label-text">{key}</span>
-                  <div className="relative">
-                    <input type="checkbox" checked={config.data?.featureFlags[key]} />
-                    <span className="toggle-mark"></span>
-                  </div>
-                </label>
-              </div>
-            </div>
-          ))}
-      </div>
     </div>
   );
 };
