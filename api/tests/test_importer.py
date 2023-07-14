0f25a23f0d1b311fe28584b93116db1698fe6ec0
Jade Wilson <jadewilson@microsoft.com>
2023-07-14 14:09:12 +0100

    add importer test back in

--- api/tests/test_importer.py
+++ api/tests/test_importer.py
@@ -45,6 +45,23 @@ async def test_import_feature_flags_from_env(mock_plugins, mock_get, mock_collec
     mock_collection.return_value.insert_one.assert_called_once()
 
 
+@mock.patch("importer.plugins.plugin_repo.collection", return_value=Mock())
+@pytest.mark.asyncio
+async def test_import_all_from_env_with_broken_plugin(mock_collection):
+    os.environ[
+        "IQENGINE_PLUGINS"
+    ] = '["name": "test_plugin", "url": "http://test_plugin"}]'
+
+    mock_collection.return_value.find_one = AsyncMock()
+    mock_collection.return_value.find_one.return_value = None
+    mock_collection.return_value.insert_one = AsyncMock()
+    mock_collection.return_value.insert_one.return_value = None
+
+    try:
+        await import_all_from_env()
+    except Exception as e:
+        e.args[0] == "Failed to load plugins from environment variable IQENGINE_PLUGINS"
+
 @mock.patch("importer.config.collection", return_value=Mock())
 @mock.patch("importer.config.get")
 @mock.patch("importer.plugins.import_plugins_from_env", return_value=None)
