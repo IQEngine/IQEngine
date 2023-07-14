2f9cc9bcf5ab1f5bc25357f7583897deef11484d
Gabriel Nepomuceno <gabriel.bico@gmail.com>
2023-07-14 10:10:08 +0100

    Display data sources

--- client/src/pages/admin/pages/data-sources.tsx
+++ client/src/pages/admin/pages/data-sources.tsx
@@ -1,14 +1,13 @@
 import { DataSource } from '@/api/Models';
 import { useGetDatasources } from '@/api/datasource/hooks/use-get-datasources';
 import React from 'react';
-import DataTable from '@/features/ui/data-table/DataTable';
 interface DataSourceRowProps {
   dataSource: DataSource;
 }
 
 export const DataSourceRow = ({ dataSource }: DataSourceRowProps) => {
   return (
-    <div className="block card w-80 bg-base-100 shadow-xl hover:bg-secondary border-secondary border-2">
+    <div className="card w-80 bg-base-100 shadow-xl border-secondary border-2">
       <figure className="p-4">
         <img src={dataSource.imageURL} alt={dataSource.description} />
       </figure>
@@ -27,7 +26,7 @@ export const DataSources = () => {
   const { apiQuery, blobQuery } = useGetDatasources();
 
   return (
-    <div className="flex flex-1">
+    <div className="flex flex-wrap gap-4">
       {apiQuery.data?.map((item, i) => (
         <DataSourceRow key={i} dataSource={item} />
       ))}
