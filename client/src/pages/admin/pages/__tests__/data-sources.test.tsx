5b61d0c9cbbd6633389fa37a7d163d4188e6a1cc
Gabriel Nepomuceno <gabriel.bico@gmail.com>
2023-07-14 13:14:50 +0100

    correcting tests

--- client/src/pages/admin/pages/__tests__/data-sources.test.tsx
+++ client/src/pages/admin/pages/__tests__/data-sources.test.tsx
@@ -3,10 +3,45 @@ import { render, screen } from '@testing-library/react';
 import DataSources from '@/pages/admin/pages/data-sources';
 import '@testing-library/jest-dom';
 import React from 'react';
+import { useAllProviders } from '@/mocks/setup-tests';
+import { ClientType, DataSource } from '@/api/Models';
 
 describe('Test DataSources', () => {
   test('Basic Rendering', async () => {
-    render(<DataSources></DataSources>);
+    const { wrapper, queryClient } = useAllProviders();
+    queryClient.setQueryData(
+      ['datasource', ClientType.BLOB],
+      [
+        {
+          type: ClientType.BLOB,
+          name: 'Test Blob',
+          description: 'Test Blob Description',
+          imageURL: 'https://test.blob.core.windows.net/test/test.png',
+          account: 'test',
+          container: 'test',
+          sasToken: 'test',
+        },
+      ]
+    );
+    queryClient.setQueryData(
+      ['datasource', ClientType.API],
+      [
+        {
+          type: ClientType.API,
+          name: 'Test API',
+          description: 'Test API Description',
+          imageURL: 'https://test.blob.core.windows.net/test/test.png',
+          account: 'test',
+          container: 'test',
+          sasToken: 'test',
+        },
+      ]
+    );
+    render(<DataSources></DataSources>, {
+      wrapper: wrapper,
+    });
     expect(await screen.findByRole('heading', { name: 'Data Sources' })).toBeInTheDocument();
+    expect(screen.getByText('Test Blob')).toBeInTheDocument();
+    expect(screen.getByText('Test API')).toBeInTheDocument();
   });
 });
