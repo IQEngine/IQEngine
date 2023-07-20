c59e6d6296d4b0cbe94ffcd059e85c91f3eae4c4
Greg Oliver <divingclimber@gmail.com>
2023-07-20 14:02:27 +0100

    add use get image hook (#365)
    * add route
    add basic page
    
    * refactor
    
    * create hook useGetImage
    create test for hook
    call hook from test page
    
    * cleanup
    fixup tests
    add debug logs to selector
    
    * rename hook source file
    
    * initial commit
    
    * add useEffect to useGetImage
    
    * fix log loop
    move hook to the correct folder
    
    * add test
    
    * Correct tests
    
    * Added test verification
    
    * npm i
    
    * compare ImageData in test
    
    * add test
    
    * add a test with average magnitude
    
    * revert
    
    * Adding more test conditions
    
    * add test with many magnitudes
    
    * many tests, parameterized
    
    * remove imageData from state
    
    * change to console.debug
    
    * remove dev test page
    remove extra comments
    remove extra logs
    
    * revert selector
    
    * remove supporting functions to utils folder
    refactor the "images are the same" test to a series of assertions
    PR feedback
    
    * remove file reference in vitest config
    
    * add vitest.setup.ts
    
    * fix bugs in tests
    add failing tests
    add small fftsize tests
    protect from 0 fftsize
    
    * add tests for normalizeMagnitude
    export normalizeMagnitude
    
    * Getting image data back
    
    * getting info from mock
    
    * modify parameter filtering for useGetImage hook
    add comments to tests, change mix of tests for new filters
    
    * shorten tilesize
    
    * respond to feedback
    
    ---------
    
    Co-authored-by: greg oliver <golive@microsoft.com>
    Co-authored-by: Gabriel Nepomuceno <gabriel.bico@gmail.com>

---
+++ client/src/pages/spectrogram/hooks/use-get-image.tsx
@@ -0,0 +1,49 @@
+import { fftToRGB } from '@/utils/selector';
+import { colMaps } from '@/utils/colormap';
+import { useEffect, useState } from 'react';
+
+export const useGetImage = (
+  totalFftData: Float32Array,
+  fftSize: number,
+  magnitudeMin: number,
+  magnitudeMax: number,
+  colmap: string
+) => {
+  const [image, setImage] = useState<ImageBitmap>(null);
+
+  useEffect(() => {
+    if (
+      !totalFftData ||
+      !fftSize ||
+      isNaN(magnitudeMin) ||
+      isNaN(magnitudeMax) ||
+      magnitudeMin >= magnitudeMax ||
+      !colmap
+    ) {
+      setImage(null);
+    } else {
+      const rgbData = fftToRGB(totalFftData, fftSize, magnitudeMin, magnitudeMax, colMaps[colmap]);
+      let num_final_ffts = totalFftData.length / fftSize;
+      const newImageData = new ImageData(rgbData, fftSize, num_final_ffts);
+
+      createImageBitmap(newImageData).then((imageBitmap) => {
+        setImage(imageBitmap);
+      });
+
+      console.debug(
+        'size of totalFftData:',
+        totalFftData.length,
+        'fftSize:',
+        fftSize,
+        ', magnitudeMin:',
+        magnitudeMin,
+        ', magnitudeMax:',
+        magnitudeMax,
+        ', num_final_ffts:',
+        num_final_ffts
+      );
+    }
+  }, [totalFftData, fftSize, magnitudeMin, magnitudeMax, colmap]);
+
+  return { image };
+};
