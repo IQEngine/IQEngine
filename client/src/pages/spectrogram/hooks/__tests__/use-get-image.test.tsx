904eaa326648d881a17164ab08628a3625c4443e
greg oliver <golive@microsoft.com>
2023-07-20 11:40:00 +0100

    respond to feedback

--- client/src/pages/spectrogram/hooks/__tests__/use-get-image.test.tsx
+++ client/src/pages/spectrogram/hooks/__tests__/use-get-image.test.tsx
@@ -38,22 +38,25 @@ describe('DevTest Spectrogram Tests', () => {
     ['min>max', tilesize, 256, -20.0, -30.0, 'jet', SampleType.WhiteBox],
     ['min==max', tilesize, 512, -60.0, -60.0, 'plasma', SampleType.WhiteBox],
     ['fftSize==0', tilesize, 0, -40.0, -10.0, 'jet', SampleType.WhiteBox],
-  ])('Failing test: %s', async (comment, tile_size, fftSize, magnitudeMin, magnitudeMax, colorMap, sampleType) => {
-    const { sampleRecording } = generateSampleRecording(
-      tile_size,
-      fftSize,
-      sampleType,
-      magnitudeMin,
-      magnitudeMax,
-      colorMap
-    );
+  ])(
+    'Function parameter tests catch these errors: %s',
+    async (comment, tile_size, fftSize, magnitudeMin, magnitudeMax, colorMap, sampleType) => {
+      const { sampleRecording } = generateSampleRecording(
+        tile_size,
+        fftSize,
+        sampleType,
+        magnitudeMin,
+        magnitudeMax,
+        colorMap
+      );
 
-    // run the code-under-test
-    const { result } = renderHook(() => useGetImage(sampleRecording, fftSize, magnitudeMin, magnitudeMax, colorMap));
-    await waitFor(() => {
-      expect(result.current.image).toBeNull();
-    });
-  });
+      // run the code-under-test
+      const { result } = renderHook(() => useGetImage(sampleRecording, fftSize, magnitudeMin, magnitudeMax, colorMap));
+      await waitFor(() => {
+        expect(result.current.image).toBeNull();
+      });
+    }
+  );
 
   test.each([
     ['128,multi-dB', tilesize, 128, -40.0, 0.0, COLORMAP_DEFAULT, SampleType.MultipleBuckets],
@@ -77,43 +80,46 @@ describe('DevTest Spectrogram Tests', () => {
     ['4096,-20/20,white box', tilesize, 4096, -20.0, 20.0, 'plasma', SampleType.MultipleBuckets],
     ['8192,-60/50,inferno,white box', tilesize, 8192, -60.0, 50.0, 'inferno', SampleType.MultipleBuckets],
     ['16384,-100/50,white box', tilesize, 16384, -100.0, 50.0, 'inferno', SampleType.MultipleBuckets],
-  ])('Passing test: %s', async (comment, tile_size, fftSize, magnitudeMin, magnitudeMax, colorMap, sampleType) => {
-    const { sampleRecording, num_ffts, expectedImageData } = generateSampleRecording(
-      tile_size,
-      fftSize,
-      sampleType,
-      magnitudeMin,
-      magnitudeMax,
-      colorMap
-    );
+  ])(
+    'RGB is generated correctly from the following fftdata: %s',
+    async (comment, tile_size, fftSize, magnitudeMin, magnitudeMax, colorMap, sampleType) => {
+      const { sampleRecording, num_ffts, expectedImageData } = generateSampleRecording(
+        tile_size,
+        fftSize,
+        sampleType,
+        magnitudeMin,
+        magnitudeMax,
+        colorMap
+      );
 
-    // run the code-under-test
-    const { result } = renderHook(() => useGetImage(sampleRecording, fftSize, magnitudeMin, magnitudeMax, colorMap));
-    await waitFor(() => {
-      expect(result.current.image).not.toBeNull();
-    });
+      // run the code-under-test
+      const { result } = renderHook(() => useGetImage(sampleRecording, fftSize, magnitudeMin, magnitudeMax, colorMap));
+      await waitFor(() => {
+        expect(result.current.image).not.toBeNull();
+      });
 
-    // get the ImageData of the code-under-test ImageBitmap
-    const lastCall = createImageBitmap.mock.lastCall;
-    const imageDataCalled = lastCall[0];
+      // get the ImageData of the code-under-test ImageBitmap
+      const lastCall = createImageBitmap.mock.lastCall;
+      const imageDataCalled = lastCall[0];
 
-    // code-under-test should generate an image of the same height and width
-    expect(result.current.image.width).toEqual(fftSize);
-    expect(result.current.image.height).toEqual(num_ffts);
+      // code-under-test should generate an image of the same height and width
+      expect(result.current.image.width).toEqual(fftSize);
+      expect(result.current.image.height).toEqual(num_ffts);
 
-    // the expectedImageData and code-under-test ImageData should be the same
-    expect(expectedImageData.data.length).toEqual(imageDataCalled.data.length);
+      // the expectedImageData and code-under-test ImageData should be the same
+      expect(expectedImageData.data.length).toEqual(imageDataCalled.data.length);
 
-    // generate random test indices
-    let testIndices = [];
-    for (let i = 0; i < 100; i++) {
-      testIndices[i] = Math.floor(Math.random() * expectedImageData.data.length);
+      // generate random test indices
+      let testIndices = [];
+      for (let i = 0; i < 100; i++) {
+        testIndices[i] = Math.floor(Math.random() * expectedImageData.data.length);
+      }
+
+      for (let i = 0; i < 100; i++) {
+        expect(expectedImageData.data[testIndices[i]]).toEqual(imageDataCalled.data[testIndices[i]]);
+      }
     }
-
-    for (let i = 0; i < 100; i++) {
-      expect(expectedImageData.data[testIndices[i]]).toEqual(imageDataCalled.data[testIndices[i]]);
-    }
-  });
+  );
 
   test.each([
     [null, 1024, -40.0, -10.0, COLORMAP_DEFAULT],
