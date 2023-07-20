9cb806c9a0ebbd310107c74de5e4d70881339958
Gabriel Nepomuceno <gabriel.bico@gmail.com>
2023-07-20 07:26:59 +0100

    Add more user event changes

--- client/src/pages/metadata-query/__tests__/date-query.test.tsx
+++ client/src/pages/metadata-query/__tests__/date-query.test.tsx
@@ -1,5 +1,5 @@
 import { describe, expect, test, vi } from 'vitest';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import React from 'react';
 
--- client/src/pages/metadata-query/__tests__/string-query.test.tsx
+++ client/src/pages/metadata-query/__tests__/string-query.test.tsx
@@ -1,17 +1,17 @@
 import { describe, expect, test, vi } from 'vitest';
 import { render, screen, fireEvent } from '@testing-library/react';
 
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 
 import StringQuery from '@/pages/metadata-query/string-query';
 
 describe('Test StringQuery', () => {
-
   test('Basic Rendering', () => {
     render(
       <StringQuery
-        queryName='test query'
-        description='test description'
+        queryName="test query"
+        description="test description"
         validator={vi.fn()}
         handleQueryValid={vi.fn()}
         handleQueryInvalid={vi.fn()}
@@ -26,35 +26,35 @@ describe('Test StringQuery', () => {
 
     render(
       <StringQuery
-        queryName='test query'
-        description='test description'
+        queryName="test query"
+        description="test description"
         validator={validatorMock}
         handleQueryValid={handleQueryValidMock}
         handleQueryInvalid={vi.fn()}
       />
     );
     const inputElement = screen.getByTestId('string-input');
-    fireEvent.change(inputElement, { target: { value: 'testing' } });
+    await userEvent.type(inputElement, 'testing');
     expect(validatorMock).toHaveBeenCalledWith('testing');
     expect(handleQueryValidMock).toHaveBeenCalledTimes(1);
-  })
+  });
 
-  test('Typing invalid text into the input calls the validator function and handleQueryInValid', () => {
+  test('Typing invalid text into the input calls the validator function and handleQueryInValid', async () => {
     const validatorMock = vi.fn(() => false);
     const handleQueryInvalidMock = vi.fn();
 
     render(
       <StringQuery
-        queryName='test query'
-        description='test description'
+        queryName="test query"
+        description="test description"
         validator={validatorMock}
         handleQueryValid={vi.fn()}
         handleQueryInvalid={handleQueryInvalidMock}
       />
     );
     const inputElement = screen.getByTestId('string-input');
-    fireEvent.change(inputElement, { target: { value: 'testing' } });
+    await userEvent.type(inputElement, 'testing');
     expect(validatorMock).toHaveBeenCalledWith('testing');
     expect(handleQueryInvalidMock).toHaveBeenCalledTimes(1);
-  })
+  });
 });
