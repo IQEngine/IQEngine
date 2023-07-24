59997de6c196c899fae06fe150ffbc441a3d9a38
Gabriel Nepomuceno <gabriel.bico@gmail.com>
2023-07-24 19:02:03 +0100

    new data loaded

--- client/src/mocks/setup-tests.tsx
+++ client/src/mocks/setup-tests.tsx
@@ -5,6 +5,8 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { FeatureFlagsProvider } from '@/hooks/use-feature-flags';
 import { vi } from 'vitest';
 import { Toaster } from 'react-hot-toast';
+import { Annotation, CaptureSegment, SigMFMetadata } from '@/utils/sigmfMetadata';
+import { ClientType } from '@/api/Models';
 
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
@@ -35,6 +37,61 @@ export const queryClient = new QueryClient({
   },
 });
 
+export function getValidMetadata(
+  // 1 MHz sample rate
+  sampleRate: number = 1024 * 1024,
+  coreFrequency: number = 8486285000,
+  type: string = ClientType.API,
+  account: string = 'testaccount',
+  container: string = 'testcontainer',
+  filePath: string = 'test_file_path',
+  dataType: string = 'cf32_le',
+  // 128 seconds of data
+  sampleLength = 1024 * 1024 * 128
+): SigMFMetadata {
+  let meta = {
+    global: {
+      'core:datatype': dataType,
+      'core:sample_rate': sampleRate,
+      'core:version': '0.0.2',
+      'core:sha512':
+        'bb2f1f9222b172373e81d333a11a866d56611308fd481c7f9c2462e50fec62da1bddd93a94cd9b3e00dcaa6ba4ffe4546022aa50385bc582fc8dd7426740b564',
+      'core:description': '',
+      'core:author': 'Marc',
+      'core:recorder': 'GNU Radio 3.8.2',
+      'core:license': 'https://creativecommons.org/licenses/by/4.0/',
+      'traceability:revision': 1,
+      'traceability:sample_length': sampleLength,
+      'traceability:origin': {
+        type: type,
+        account: account,
+        container: container,
+        file_path: filePath,
+      },
+    },
+    captures: [
+      {
+        'core:sample_start': 0,
+        'core:frequency': coreFrequency,
+        'core:datetime': '2020-12-20T17:32:07.142626',
+      },
+    ],
+    annotations: [
+      {
+        'core:sample_start': 260780,
+        'core:sample_count': 285354,
+        'core:freq_lower_edge': 8486138750,
+        'core:freq_upper_edge': 8486243700,
+        'core:label': 'first',
+      },
+    ],
+  };
+  const metadata = Object.assign(new SigMFMetadata(), meta);
+  metadata.annotations = meta.annotations.map((annotation) => Object.assign(new Annotation(), annotation));
+  metadata.captures = meta.captures.map((capture) => Object.assign(new CaptureSegment(), capture));
+  return metadata;
+}
+
 export const AllProviders = ({ children }) => (
   <QueryClientProvider client={queryClient}>
     <FeatureFlagsProvider flags={null}>
@@ -50,5 +107,6 @@ export const useAllProviders = () => {
   return {
     wrapper: AllProviders,
     queryClient,
+    getValidMetadata,
   };
 };
---
+++ client/src/pages/spectrogram/hooks/use-spectrogram.tsx
@@ -0,0 +1,41 @@
+import { useMeta } from '@/api/metadata/Queries';
+import { useCallback, useMemo, useState } from 'react';
+
+interface SpectrogramProps {
+  fftSize: number;
+  type: string;
+  account: string;
+  container: string;
+  filePath: string;
+}
+
+export function useSpectrogram({ fftSize, type, account, container, filePath }: SpectrogramProps) {
+  const { data: meta, isSuccess: hasMetadata } = useMeta(type, account, container, filePath);
+  const total_ffts = Math.ceil(meta?.getTotalSamples() / fftSize);
+  const [currentFFT, setCurrentFFT] = useState<number>(0);
+  // This is how many ffts we skip when decimating
+  const [fftStepSize, setFFTStepSize] = useState<number>(0);
+  // This is how many ffts we display
+  const [spectrogramHeight, setSpectrogramHeight] = useState<number>(0);
+
+  // This is the list of ffts we display
+  const displayedFFTs = useMemo<number[]>(() => {
+    const ffts = [currentFFT];
+    for (let i = 1; i < spectrogramHeight; i++) {
+      ffts.push(currentFFT + i * (fftStepSize + 1));
+    }
+    return ffts;
+  }, [fftSize, currentFFT, fftStepSize, total_ffts, spectrogramHeight]);
+
+  return {
+    total_ffts,
+    currentFFT,
+    setCurrentFFT,
+    hasMetadata,
+    fftStepSize,
+    setFFTStepSize,
+    spectrogramHeight,
+    setSpectrogramHeight,
+    displayedFFTs,
+  };
+}
