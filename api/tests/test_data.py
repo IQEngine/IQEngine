6d6c16c1f13663475229d013734adc4faad51f0c
Paul Foster <paulfo@microsoft.com>
2023-07-26 16:05:08 +0100

    Test data fix

--- api/tests/test_data.py
+++ api/tests/test_data.py
@@ -13,23 +13,60 @@ test_datasource_id = f'{test_datasource["account"]}_{test_datasource["container"
 
 
 valid_metadata = {
-    "global": {
-        "core:datatype": "string",
-        "core:sample_rate": 1,
-        "core:version": "1.0.0",
-    },
-    "captures": [
-        {
-            "core:sample_start": 1,
-        }
-    ],
-    "annotations": [
-        {
-            "core:sample_start": 1,
-            "core:sample_count": 1,
-        }
-    ],
-}
+        
+        "global": {
+            "antenna:gain": None,
+            "antenna:type": None,
+            "core:author": None,
+            "core:collection": None,
+            "core:data_doi": None,
+            "core:dataset": None,
+            "core:datatype": "rf",
+            "core:description": None,
+            "core:extensions": None,
+            "core:geolocation": None,
+            "core:hw": None,
+            "core:license": None,
+            "core:meta_doi": None,
+            "core:metadata_only": None,
+            "core:num_channels": None,
+            "core:offset": None,
+            "core:recorder": None,
+            "core:sample_rate": 1000000,
+            "core:sha512": None,
+            "core:trailing_bytes": None,
+            "core:version": "0.0.1",
+            "traceability:origin": {
+                "type": "api",
+                "account": "test_account",
+                "container": "test_container",
+                "file_path": "test_file_path",
+            },
+            "traceability:revision": None,
+            "traceability:sample_length": None,
+        },
+        "captures": [
+            {
+                "core:sample_start": 0,
+                "core:global_index": 0,
+                "core:header_bytes": 0,
+                "core:frequency": 8486285000.0,
+                "core:datetime": "2020-12-20T17:32:07.142626",
+            }
+        ],
+        "annotations": [
+            {
+                "core:comment": None,
+                "core:freq_lower_edge": None,
+                "core:freq_upper_edge": None,
+                "core:generator": None,
+                "core:label": None,
+                "core:sample_count": 0,
+                "core:sample_start": 0,
+                "core:uuid": None,
+            }
+        ],
+    }
 
 
 valid_metadata_array = [
