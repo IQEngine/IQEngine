0756312669b5ab62c4415dedd16608eefe94d540
Paul Foster <paulfo@microsoft.com>
2023-07-27 17:27:48 +0100

    Test fix

--- api/tests/test_query_api.py
+++ api/tests/test_query_api.py
@@ -2,37 +2,23 @@ from unittest.mock import MagicMock
 
 import pytest
 from database import metadata_repo
-
+from tests.test_data import test_datasource, valid_metadata
 from .test_data import valid_metadata_array, valid_datasourcereference_array
-
-
-def override_metadata_collection():
-    async def async_generator():
-        for item in valid_metadata_array:
-            yield item
-
-    mock_collection = MagicMock()
-    mock_collection.find.return_value = async_generator()
-    return mock_collection
 
 
 @pytest.mark.asyncio
 async def test_query_meta_success(client):
-    account = "test_account"
-    container = "test_container"
     query_condition = "min_frequency=8486280000&max_frequency=8486290000"
 
-    # Override the dependency
-    client.app.dependency_overrides[
-        metadata_repo.collection
-    ] = override_metadata_collection
-
+    client.post("/api/datasources", json=test_datasource).json()
+    response = client.post(
+        f'/api/datasources/{test_datasource["account"]}/{test_datasource["container"]}/file_path/meta',
+        json=valid_metadata,
+    )
+    assert response.status_code == 201
     response = client.get(
-        f"/api/datasources/query?account={account}&container={container}&{query_condition}"
+        f"/api/datasources/query?{query_condition}"
     )
 
     assert response.status_code == 200
     assert response.json() == valid_datasourcereference_array
-
-    # Reset the dependency overrides after the test
-    client.app.dependency_overrides = {}
