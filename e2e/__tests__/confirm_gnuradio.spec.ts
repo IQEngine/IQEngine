57809d1aa3f1762c44fc214c8691f2288d4a52e6
Gabriel Monteiro Nepomuceno <gabriel.bico@gmail.com>
2023-07-28 14:00:30 +0100

    Conditional load datasources only if the api it is not available (#402)
    * Conditional load datasources only if the api it is not available
    
    * Add test consistency
    
    * Trying to fix playwright tests again

--- e2e/__tests__/confirm_gnuradio.spec.ts
+++ e2e/__tests__/confirm_gnuradio.spec.ts
@@ -9,7 +9,7 @@ test('GNURadio repocard displays without sas token @CICompatible', async ({ page
 test('GNURadio repocard displays spectrogram page @CICompatible', async ({ page }) => {
   await page.goto('/');
 
-  await page.locator('#GNURadioSigMFRepo').click();
+  await page.locator('#GNURadioSigMFRepo').last().click();
   await page.getByRole('link', { name: 'analog_FM_France' }).click();
   await expect(page.getByText('Zoom Level')).toBeVisible();
   await expect(page.locator('span:has-text("1.0.0")')).toBeHidden();
