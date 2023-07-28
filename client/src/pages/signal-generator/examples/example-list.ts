435f88993e6c5d16756503847dd320fab00434da
Gabriel Monteiro Nepomuceno <gabriel.bico@gmail.com>
2023-07-28 09:46:33 +0100

    Almost finishing the rename client (#401)
    * almost finishing the rename
    
    * Fixing references

--- client/src/pages/signal-generator/examples/example-list.ts
+++ client/src/pages/signal-generator/examples/example-list.ts
@@ -1,6 +1,6 @@
-import { bpskToneNoise } from './bpskToneNoise';
+import { bpskToneNoise } from './bpsk-tone-noise';
 import { ofdm } from './ofdm';
-import { directSequenceSpreadSpectrum } from './directSequenceSpreadSpectrum';
+import { directSequenceSpreadSpectrum } from './direct-sequence-spread-spectrum';
 
 // Add yours to the end of this list, and remember to import it above
 export const examplesList = [bpskToneNoise, ofdm, directSequenceSpreadSpectrum];
