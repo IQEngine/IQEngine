a94362336d67cbd315db9b8f13d3288401e39b3b
Jade Wilson <jadewilson@microsoft.com>
2023-08-05 22:33:57 +0100

    add changes

--- client/src/pages/recording-view/components/annotation/annotation-viewer.tsx
+++ client/src/pages/recording-view/components/annotation/annotation-viewer.tsx
@@ -124,10 +124,13 @@ const AnnotationViewer = ({ currentFFT }: AnnotationViewerProps) => {
       var textarea = document.createElement('textarea');
       document.body.appendChild(textarea);
 
+      const element = document.getElementById('spectrogram');
+      const rect = element.getBoundingClientRect();
+
       textarea.value = e.target.text();
       textarea.style.position = 'absolute';
-      textarea.style.top = '300px'; // middle of screen
-      textarea.style.left = '500px'; // middle of screen
+      textarea.style.top = rect.top + e.target.attrs.y + 'px'; // middle of screen
+      textarea.style.left = rect.left + e.target.attrs.x + 'px'; // middle of screen
       textarea.style.width = '400px';
       textarea.style.fontSize = '25px';
       textarea.rows = 1;
@@ -140,8 +143,8 @@ const AnnotationViewer = ({ currentFFT }: AnnotationViewerProps) => {
       document.body.appendChild(textarea2);
       textarea2.value = 'Hit Enter to Finish';
       textarea2.style.position = 'absolute';
-      textarea2.style.top = '270px';
-      textarea2.style.left = '600px';
+      textarea2.style.top = rect.top + e.target.attrs.y - 30 + 'px';
+      textarea2.style.left = rect.left + e.target.attrs.x + 100 + 'px';
       textarea2.style.width = '140px';
       textarea2.style.height = '30px';
       textarea2.rows = 1;
