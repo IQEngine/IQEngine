e458daf5b0ea0dcda963c58012cbfe3a01770808
Hannah <hannahlspencer@gmail.com>
2023-08-09 13:17:36 +0100

    Fix Add Annotation (#450)
    * Hacky fix
    
    * Unhacky fix
    
    * �-Pipeline
    
    * �-Pipeline

--- client/src/pages/recording-view/components/annotation/annotation-viewer.tsx
+++ client/src/pages/recording-view/components/annotation/annotation-viewer.tsx
@@ -74,7 +74,7 @@ const AnnotationViewer = ({ currentFFT }: AnnotationViewerProps) => {
       };
     });
     return annotations;
-  }, [meta.annotations, currentFFT, fftStepSize, fftSize, spectrogramWidth]);
+  }, [meta, currentFFT, fftStepSize, fftSize, spectrogramWidth]);
 
   // add cursor styling
   function onMouseOver() {
