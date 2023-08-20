8f03cecdd0f5e6394d48b3ad6d75f0fa864d9d8d
Marc Lichtman <marcll@vt.edu>
2023-08-20 16:30:41 -0400

    pause after loading page

--- e2e/__tests__/confirm_validator_card.spec.ts
+++ e2e/__tests__/confirm_validator_card.spec.ts
@@ -3,6 +3,8 @@ import { test } from '@playwright/test';
 test('Confirm Validator and AzureBlob cards @CICompatible', async ({ page }) => {
   await page.goto('/browser');
 
+  await page.waitForTimeout(1000);
+
   // signal generator
   await page.locator('[id="Validator"]').click();
   await page.locator('[id="IQEngineLogo"]').click();
