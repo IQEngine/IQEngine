457868f69dde839a65d9027a78ac2cd23cac5a91
Marc Lichtman <marcll@vt.edu>
2023-08-23 13:13:50 -0400

    fixed test

--- e2e/__tests__/confirm_validator_card.spec.ts
+++ e2e/__tests__/confirm_validator_card.spec.ts
@@ -6,11 +6,11 @@ test('Confirm Validator and AzureBlob cards @CICompatible', async ({ page }) => 
   // signal generator
   await page.locator('[id="Validator"]').click();
 
-  // Go to landing page
+  // Go to landing page, but by default its bypassed so it will redirect to the browser page
   await page.locator('[id="IQEngineLogo"]').click();
 
   // Go back to browser page
-  await page.locator('[id="browse-button"]').click();
+  //await page.locator('[id="browse-button"]').click();
 
   // Azure blob
   await page.locator('[id="AzureBlob"]').click();
