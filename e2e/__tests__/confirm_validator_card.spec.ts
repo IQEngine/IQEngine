a33dff901611950a0fbd227ad528fa9d7140e019
Marc Lichtman <marcll@vt.edu>
2023-08-23 20:40:54 -0400

    redoing validator test

--- e2e/__tests__/confirm_validator_card.spec.ts
+++ e2e/__tests__/confirm_validator_card.spec.ts
@@ -1,17 +1,8 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 test('Confirm Validator and AzureBlob cards @CICompatible', async ({ page }) => {
   await page.goto('/');
   await page.waitForTimeout(500); // bypass landing page is on by default but takes a moment
-
-  // signal generator
   await page.locator('[id="Validator"]').click();
-
-  // Go to landing page, but by default its bypassed so it will redirect to the browser page
-  await page.locator('[id="IQEngineLogo"]').click();
-  await page.waitForNavigation();
-  await page.waitForTimeout(5000); // bypass landing page is on by default but takes a moment
-
-  // Azure blob
-  await page.locator('[id="AzureBlob"]').click();
+  await expect(page.getByText('"global"', { exact: true })).toBeVisible();
 });
