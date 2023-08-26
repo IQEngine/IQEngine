c6651934bde61c354dc462b9ecf1d87508a15270
Marc Lichtman <marcll@vt.edu>
2023-08-26 15:53:10 -0400

    Fixed time iq freq width and height (#586)
    * fixed time iq freq width and height
    
    * top level permissions

--- client/src/pages/recording-view/components/time-plot.tsx
+++ client/src/pages/recording-view/components/time-plot.tsx
@@ -6,9 +6,10 @@ import Plot from 'react-plotly.js';
 import React, { useEffect, useState } from 'react';
 import { template } from '@/utils/plotlyTemplate';
 import { useCursorContext } from '../hooks/use-cursor-context';
+import { useSpectrogramContext } from '../hooks/use-spectrogram-context';
 
-export const TimePlot = (props) => {
-  let { plotWidth, plotHeight } = props;
+export const TimePlot = () => {
+  const { spectrogramWidth, spectrogramHeight } = useSpectrogramContext();
   const { cursorData } = useCursorContext();
   const [I, setI] = useState<Float32Array>();
   const [Q, setQ] = useState<Float32Array>();
@@ -53,8 +54,8 @@ export const TimePlot = (props) => {
           },
         ]}
         layout={{
-          width: plotWidth,
-          height: plotHeight,
+          width: spectrogramWidth,
+          height: spectrogramHeight,
           dragmode: 'pan',
           showlegend: true,
           template: template,
