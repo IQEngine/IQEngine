d0de0aa1e1f5457a9f78e8a837c9e0f887efd357
Marc Lichtman <marcll@vt.edu>
2023-08-26 13:41:19 -0400

    increase delay

--- e2e/common-steps.ts
+++ e2e/common-steps.ts
@@ -1,6 +1,6 @@
 export async function skipLandingPage(page) {
   // If landing page is enabled, click the browse button
-  await page.waitForTimeout(500); // bypass landing page is on by default but takes a moment
+  await page.waitForTimeout(1000); // bypass landing page is on by default but takes a moment. 500ms was too short for consistency on github actions
   if (await page.isVisible('[id="browse-button"]')) {
     await page.locator('[id="browse-button"]').click();
     await page.waitForTimeout(500);
