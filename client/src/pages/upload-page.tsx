20dac98e75b6b733a5def807d74d10f536cdddda
Marc Lichtman <marcll@vt.edu>
2023-08-27 14:27:05 -0400

    working

--- client/src/pages/upload-page.tsx
+++ client/src/pages/upload-page.tsx
@@ -4,7 +4,7 @@
 
 import React, { useState } from 'react';
 import { useConfigQuery } from '@/api/config/queries';
-import { ContainerClient } from '@azure/storage-blob';
+import { ContainerClient, newPipeline, AnonymousCredential, BlockBlobClient } from '@azure/storage-blob';
 import { fileOpen } from 'browser-fs-access';
 
 function readFile(file: Blob): Promise<ArrayBuffer> {
@@ -30,19 +30,44 @@ export const UploadPage = () => {
   const config = useConfigQuery();
 
   async function uploadBlob(f) {
-    const containerClient = new ContainerClient(config.data.uploadPageBlobSasUrl);
-    let content = await readFile(f);
+    //let content = await readFile(f);
 
+    // Create azure blob client
     const blobName = f.name.split('.')[0] + '_' + new Date().toISOString().split('.')[0] + '.' + f.name.split('.')[1];
-    let blockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+    const fullBlobURL =
+      'https://gnuradio.blob.core.windows.net/triage/' +
+      blobName +
+      '?sp=racwdli&st=2023-08-27T18:14:28Z&se=2023-09-28T02:14:28Z&sv=2022-11-02&sr=c&sig=84AXbsLwge1EDEu20FQ3N8e%2BKIcpVm91pLWUv%2FFPnro%3D';
+    console.log(fullBlobURL);
+    const pipeline = newPipeline(new AnonymousCredential());
+    const blockBlobClient = new BlockBlobClient(fullBlobURL, pipeline);
+
+    // chunking related
+    const blockSize = 1 * 1024 * 1024; // 1MB
+    const blockCount = Math.ceil(f.size / blockSize);
+    console.log(blockCount, 'blocks');
+    const blockIds = [];
+    for (let i = 0; i < blockCount; i++) {
+      console.log(i);
+      const start = i * blockSize;
+      const end = Math.min(start + blockSize, f.size);
+      const chunk = f.slice(start, end);
+      const chunkSize = end - start;
+      const blockId = btoa('block-' + i.toString().padStart(6, '0'));
+      blockIds.push(blockId);
+      await blockBlobClient.stageBlock(blockId, chunk, chunkSize);
+    }
+    await blockBlobClient.commitBlockList(blockIds);
+    console.log('done uploading ' + f.name);
 
     // Create a new blob
-    try {
-      const resp = await blockBlobClient.upload(content, f.size);
-      console.log(resp);
-    } catch (error) {
-      console.error(error);
-    }
+    //try {
+    //  const resp = await blockBlobClient.upload(content, f.size);
+    //  console.log(resp);
+    //} catch (error) {
+    //  console.error(error);
+    //}
   }
 
   const openFiles = async () => {
