b3be34fb04112f2396be59522ff657172ef5dd14
Marc Lichtman <marcll@vt.edu>
2023-09-22 22:17:43 -0400

    useless comments

--- api/rf/samples.py
+++ api/rf/samples.py
@@ -1,23 +1,6 @@
 import numpy as np
 
-
 def get_samples(data_bytes, data_type) -> np.ndarray:
-    """
-    Get samples from bytes.
-
-    Parameters
-    ----------
-    data_bytes : bytes
-        The bytes to convert to samples.
-    data_type : str
-        The data type of the bytes.
-
-    Returns
-    -------
-    np.ndarray
-        The samples.
-    """
-
     if data_type == "ci8" or data_type == "i8":
         samples = np.frombuffer(data_bytes, dtype=np.int8)
         samples = samples[::2] + 1j * samples[1::2]
@@ -47,22 +30,7 @@ def get_samples(data_bytes, data_type) -> np.ndarray:
         raise ValueError("Datatype " + data_type + " not implemented")
     return samples
 
-
 def get_bytes_per_iq_sample(data_type):
-    """
-    Get the number of bytes per I+Q sample.
-
-    Parameters
-    ----------
-    data_type : str
-        The data type of the bytes.
-
-
-    Returns
-    -------
-    int
-        The number of bytes per I+Q sample.
-    """
     if data_type == "cf32_le" or data_type == "cf32" or data_type == "cf32_be":
         return 8
     elif data_type == "ci16_le" or data_type == "ci16" or data_type == "ci16_be":
