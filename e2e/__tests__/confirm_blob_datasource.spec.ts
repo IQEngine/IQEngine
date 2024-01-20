bb8aa69939d5cc9589280c4f4beca0a7eb78c0fb
Marc Lichtman <marcll@vt.edu>
2024-01-19 23:28:53 -0500

    fixed a e2e test

--- e2e/__tests__/confirm_blob_datasource.spec.ts
+++ e2e/__tests__/confirm_blob_datasource.spec.ts
@@ -1,6 +1,6 @@
 import { test } from '@playwright/test';
 
-test('Confirm a blob datasource @CICompatible', async ({ page }) => {
+test('Confirm a blob datasource', async ({ page }) => {
   await page.goto('/browser');
   const grRepo = page.locator('#GNURadioSigMFRepo');
   const grRepo2 = page.locator('#GNURadioHostedRecordings');
