88f0eb14818f03e8cec8700ccafd022c0668712e
Marc Lichtman <marcll@vt.edu>
2024-02-19 19:27:13 -0500

    renaming

--- client/src/pages/recording-view/hooks/use-spectrogram.tsx
+++ client/src/pages/recording-view/hooks/use-spectrogram.tsx
@@ -41,54 +41,55 @@ export function useSpectrogram(currentFFT) {
       return null;
     }
 
-    // get the current required and displayed blocks
-    const requiredBlocks: number[] = []; // used alongside setFFTsRequired()
-    const displayedBlocks: number[] = [];
+    // get the current required and displayed FFT indices
+    const requiredFFTIndices: number[] = []; // used alongside setFFTsRequired()
+    const displayedFFTIndices: number[] = [];
     for (let i = 0; i < spectrogramHeight; i++) {
-      const nextFFT = currentFFT + i * (fftStepSize + 1);
-      if (nextFFT <= totalFFTs && nextFFT >= 0) {
-        requiredBlocks.push(nextFFT);
-        displayedBlocks.push(nextFFT);
+      const nextFFT = currentFFT + i * (fftStepSize + 1); // currentFFT is just the index of the fft corresponding to the top of the spectrogram
+      if (nextFFT > totalFFTs || nextFFT < 0) {
+        console.log('[use-spectrogram] SHOULDNt HAVE GOT HERE!!! C'); // doesnt normally get here
+        continue;
       }
+      requiredFFTIndices.push(nextFFT);
+      displayedFFTIndices.push(nextFFT);
     }
+    console.log('********', displayedFFTIndices);
 
-    // add the padding to requiredBlocks
+    // add the padding to requiredFFTIndices
     const currentPadding = Math.floor(FETCH_PADDING / (fftSize / 1024)); // make the padding (which is in units of ffts) a function of the size of fft so we avoid to fetch too much data for large ffts, this was manually tweaked
     for (let i = 1; i <= currentPadding; i++) {
-      let start = displayedBlocks[0];
-      let end = displayedBlocks[displayedBlocks.length - 1];
+      let start = displayedFFTIndices[0];
+      let end = displayedFFTIndices[displayedFFTIndices.length - 1];
       let step = i * (fftStepSize + 1);
       // The padding gets added to the beginning and end of the visible ffts on the screen. it cant go beyond recording size or negative
-      if (start - step >= 0) requiredBlocks.push(start - step);
-      if (end + step <= totalFFTs) requiredBlocks.push(end + step);
+      if (start - step >= 0) requiredFFTIndices.push(start - step);
+      if (end + step <= totalFFTs) requiredFFTIndices.push(end + step);
     }
 
     if (!currentData || Object.keys(currentData).length === 0) {
-      setFFTsRequired(requiredBlocks);
+      setFFTsRequired(requiredFFTIndices);
       return null;
     }
 
-    // check if the blocks are already loaded
-    const blocksToLoad = requiredBlocks.filter((block) => !currentData[block]);
-    setFFTsRequired(blocksToLoad);
+    // check if the FFT are already loaded
+    setFFTsRequired(requiredFFTIndices.filter((i) => !currentData[i]));
 
-    // return the data with 0s for the missing blocks
+    // return the data with 0s for the missing FFTs
     const iqData = new Float32Array(spectrogramHeight * fftSize * 2);
-    let offset = 0;
     for (let i = 0; i < spectrogramHeight; i++) {
-      if (currentData[displayedBlocks[i]]) {
-        if (currentData[displayedBlocks[i]].length + offset > iqData.length) {
+      if (currentData[displayedFFTIndices[i]]) {
+        if (currentData[displayedFFTIndices[i]].length + i * fftSize * 2 > iqData.length) {
+          console.log('[use-spectrogram] SHOULDNt HAVE GOT HERE!!! A'); // doesnt normally get here
           continue;
         }
-        iqData.set(currentData[displayedBlocks[i]], offset);
+        iqData.set(currentData[displayedFFTIndices[i]], i * fftSize * 2);
       } else {
-        if (offset + fftSize * 2 > iqData.length) {
-          //console.log('GOT HERE A'); // doesnt normally get here
+        if ((i + 1) * fftSize * 2 > iqData.length) {
+          console.log('[use-spectrogram] SHOULDNt HAVE GOT HERE!!! B'); // doesnt normally get here
           continue;
         }
-        iqData.fill(-Infinity, offset, offset + fftSize * 2);
+        iqData.fill(-Infinity, i * fftSize * 2, (i + 1) * fftSize * 2);
       }
-      offset += fftSize * 2;
     }
     return iqData;
   }, [
