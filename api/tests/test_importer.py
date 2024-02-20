8cf987ad290f8dad38ee2aaec05fedd17d815177
Marc Lichtman <marcll@vt.edu>
2024-02-20 01:10:53 -0500

    api unit tests

--- api/tests/test_importer.py
+++ api/tests/test_importer.py
@@ -69,32 +69,6 @@ async def test_import_all_from_env_with_broken_plugin(mock_collection):
 @mock.patch("app.plugins.import_plugins_from_env", return_value=None)
 @mock.patch("app.datasources.import_datasources_from_env", return_value=None)
 @pytest.mark.asyncio
-async def test_import_feature_flags_from_env_update(
-    mock_datasource, mock_plugins, mock_get, mock_collection
-):
-    os.environ[
-        "IQENGINE_PLUGINS"
-    ] = '[{"name": "test_plugin", "url": "http://test_plugin"}]'
-    os.environ["IQENGINE_FEATURE_FLAGS"] = '{"test": true}'
-
-    test_config = Configuration()
-    test_config.feature_flags = None
-    mock_get.return_value = test_config
-
-    mock_collection.return_value.update_one = AsyncMock()
-    mock_collection.return_value.insert_one = AsyncMock()
-
-    await import_all_from_env()
-
-    mock_collection.return_value.update_one.assert_called_once()
-    mock_collection.return_value.insert_one.assert_not_called()
-
-
-@mock.patch("app.config.collection", return_value=Mock())
-@mock.patch("app.config.get")
-@mock.patch("app.plugins.import_plugins_from_env", return_value=None)
-@mock.patch("app.datasources.import_datasources_from_env", return_value=None)
-@pytest.mark.asyncio
 async def test_import_feature_flags_from_env_no_insert_or_update(
     mock_datasource, mock_plugins, mock_get, mock_collection
 ):
