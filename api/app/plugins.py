3a8f26ebe546dfcd8eac1e6943d5210abd567f06
Marc Lichtman <marcll@vt.edu>
2024-03-01 19:36:10 -0500

    clear plugins collection before importing

--- api/app/plugins.py
+++ api/app/plugins.py
@@ -2,37 +2,23 @@ import json
 import os
 from .database import db
 from .models import Plugin
-from motor.core import AgnosticCollection
-
-def collection() -> AgnosticCollection:
-    collection: AgnosticCollection = db().plugins
-    return collection
 
 async def get(name) -> Plugin:
-    plugin = await collection().find_one({"name": name})
+    plugins_collection = db().plugins
+    plugin = await plugins_collection.find_one({"name": name})
     if not plugin:
         return None
     return Plugin(**plugin)
 
+# Import plugins from environment variable, clears the collection first
 async def import_plugins_from_env(environment_variable_name="IQENGINE_PLUGINS"):
-    """
-    Import plugins from environment variable
-    Will not import if plugin already exists
-    """
     try:
         plugins_json = os.getenv(environment_variable_name, None)
         if not plugins_json:
             return None
-        plugins = json.loads(plugins_json)
-
-        client = collection()
-        for plugin in plugins:
-            if await client.find_one({"name": plugin["name"]}, {"_id": 1}):
-                continue
-            await client.insert_one(plugin)
+        plugins_collection = db().plugins
+        await plugins_collection.delete_many({})  # clears the plugins db
+        for plugin in json.loads(plugins_json):  # list of dicts
+            await plugins_collection.insert_one(plugin)
     except Exception as e:
-        # throw a custom plugin failed to load exception
-        raise Exception(
-            f"Failed to load plugins from environment variable {environment_variable_name}",
-            e,
-        )
+        raise Exception(f"Failed to load plugins from environment variable {environment_variable_name}", e)
