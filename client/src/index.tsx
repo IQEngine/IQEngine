8fb96ebf60ae249e0454ed28b621469e6a937f50
Marc Lichtman <marcll@vt.edu>
2024-04-19 20:45:08 -0400

    remove custom use-query hook, not needed

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -8,16 +8,32 @@ import 'swagger-ui-react/swagger-ui.css';
 import reportWebVitals from '@/utils/reportWebVitals';
 import { RouterProvider } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
-import { QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { FeatureFlagsProvider } from '@/hooks/use-feature-flags';
-import { useQueryClient } from '@/hooks/use-query-client';
 import { useRouter } from '@/hooks/use-router';
 import { useAuthProvider } from '@/auth/hooks/use-auth-provider';
+import { CLIENT_TYPE_BLOB } from '@/api/Models';
 
 const container = document.getElementById('root');
 if (!container) throw new Error('No root element found');
 const root = createRoot(container);
-const { queryClient } = useQueryClient();
+
+// React Query stuff
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+queryClient.setQueryDefaults(['config'], { staleTime: 1000 * 60 * 60 * 24 });
+queryClient.setQueryDefaults(['datasource', CLIENT_TYPE_BLOB], { staleTime: 1000 * 60 });
+queryClient.setQueryDefaults(['plugins'], { staleTime: 1000 * 60 * 5 });
+queryClient.setQueryDefaults(['rawiqdata'], { staleTime: Infinity });
+queryClient.setQueryDefaults(['iqData'], { staleTime: 5, cacheTime: 10 });
+queryClient.setQueryDefaults(['user-settings'], { staleTime: Infinity });
+queryClient.setQueryDefaults(['smart-query'], { staleTime: 1000 * 60 * 60 * 24 });
+
 const { router } = useRouter();
 const { AuthProvider } = useAuthProvider();
 root.render(
@@ -30,7 +46,5 @@ root.render(
   </QueryClientProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// If you want to start measuring performance in your app, pass a function to log results (for example: reportWebVitals(console.log)) or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals(console.log);
