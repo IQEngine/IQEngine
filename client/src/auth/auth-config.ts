8ed14b11c6e1e3bac53fea6fa6a03b16f2ea9f49
Marc Lichtman <marcll@vt.edu>
2024-04-23 16:08:47 -0400

    msal suppress info/debug messages (#871)

--- client/src/auth/auth-config.ts
+++ client/src/auth/auth-config.ts
@@ -24,10 +24,10 @@ export const msalConfig = (config) => {
               console.error(message);
               return;
             case LogLevel.Info:
-              console.info(message);
+              //console.info(message);
               return;
             case LogLevel.Verbose:
-              console.debug(message);
+              //console.debug(message);
               return;
             case LogLevel.Warning:
               console.warn(message);
