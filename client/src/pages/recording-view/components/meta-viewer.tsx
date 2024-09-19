7b159ee89e97e495c5d29ae8e8b4ff6d4d5c6600
Marc Lichtman <marcll@vt.edu>
2024-09-18 22:47:26 -0400

    fixed metadata summary styling

--- client/src/pages/recording-view/components/meta-viewer.tsx
+++ client/src/pages/recording-view/components/meta-viewer.tsx
@@ -9,22 +9,22 @@ export interface MetaViewerProps {
 export const MetaViewer = ({ meta }: MetaViewerProps) => {
   if (!meta) return <></>;
   return (
-    <div className="flex justify-evenly border border-primary p-2">
-      <div className="flex">
+    <div className="flex justify-evenly border border-primary p-2 gap-2">
+      <div className="flex-col">
         <div className="text-primary mr-2 text-sm">data type:</div>
         <div className="text-base-content text-sm">{meta.getDataType()}</div>
       </div>
-      <div className="flex">
+      <div className="flex-col">
         <div className="text-primary mr-2 text-sm">sample rate:</div>
         <div className="text-base-content text-sm">
           {unitPrefixHz(meta.getSampleRate()).freq} {unitPrefixHz(meta.getSampleRate()).unit}
         </div>
       </div>
-      <div className="flex">
+      <div className="flex-col">
         <div className="text-primary mr-2 text-sm">file name:</div>
         <div className="text-base-content text-sm">{meta.getFileName()}</div>
       </div>
-      <div className="flex">
+      <div className="flex-col">
         <div className="text-primary mr-2 text-sm">description:</div>
         <div className="text-base-content text-sm">{meta.getDescription()}</div>
       </div>
