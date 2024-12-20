ef4bf0f7401cba73a7373d2f6bb693c4a2abc3c7
Marc Lichtman <marcll@vt.edu>
2024-12-20 03:39:30 -0500

    update the starter IQENGINE_PLUGINS

--- e2e/__tests__/confirm_plugins.spec.ts
+++ e2e/__tests__/confirm_plugins.spec.ts
@@ -1,17 +1,9 @@
 import { test, expect } from '@playwright/test';
 
-test.beforeAll(async ({ request }) => {
-  const plugin = {
-    name: 'test plugin server for e2e',
-    url: 'http://127.0.0.1:8000/plugins',
-  };
-  await request.post('/api/plugins', { data: plugin });
-});
-
 test('List plugins and params @CICompatible', async ({ request }) => {
   const response = await request.get(`/api/plugins`);
   // remove entry with Local as the name
-  const plugins = (await response.json()).filter((plugin) => plugin.name !== 'Local');
+  const plugins = (await response.json()).filter((plugin) => plugin.name !== 'Local' && plugin.name !== 'BuiltIn');
   console.log(plugins);
   // get url of first item
   const url = plugins[0].url;
@@ -27,7 +19,7 @@ test('List plugins and params @CICompatible', async ({ request }) => {
 
 test('Make sure GNU Radio works @CICompatible', async ({ request }) => {
   const response = await request.get(`/api/plugins`);
-  const plugins = (await response.json()).filter((plugin) => plugin.name !== 'Local');
+  const plugins = (await response.json()).filter((plugin) => plugin.name !== 'Local' && plugin.name !== 'BuiltIn');
   // eg https://iqengine-staging-plugins.agreeableforest-695e4014.eastus2.azurecontainerapps.io/plugins
   const response2 = await request.get(plugins + '/test-gnuradio');
   await expect(response2).toBeOK();
